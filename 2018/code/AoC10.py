9530e7d582e86c3ed45bd5adda80343c0fe23f53
Sten Otto Johnsen <STEOJ@statoil.com>
2018-12-10 16:00:35 +0100

    Added more unit tests

--- 2018/code/AoC10.py
+++ 2018/code/AoC10.py
@@ -30,14 +30,20 @@ class Point():
     c = []
     v = []
     def __init__(self, ptArr):
-        self.c.append(ptArr[0])
-        self.v.append(ptArr[1])
+        self.c = ptArr[0]
+        self.v = ptArr[1]
 
     def x(self):
         return self.c[0]
 
     def y(self):
         return self.c[1]
+    
+    def vx(self):
+        return self.v[0]
+
+    def vy(self):
+        return self.v[1]
 
     def dump(self):
         pp.pprint([self.c,self.v])
@@ -46,7 +52,12 @@ class Point():
         return [self.c,self.v]
 
     def manhattan(self, pt):
-        return abs(self.x-pt.x)+abs(self.y - pt.y)
+        return abs(self.x()-pt.x())+abs(self.y() - pt.y())
+
+    def moveOneSecond(self):
+        self.c[0] += self.v[0]
+        self.c[1] += self.v[1]
+        return self
 
 
 class SkyMap():
@@ -73,171 +84,12 @@ class SkyMap():
         self.load(data)
 
 
-sm = SkyMap(inData)
-sm.dump()
+# sm = SkyMap(inData)
+# sm.dump()
 
-exit()
+# print('\nPart 1: The largest area not infinite is', saveArea)
 
-for i in inData:
-    print(getPosAndVelocity(i))
-
-
-data = []
-sum = 0
-for i in range(len(inData)):
-    r = inData[i].split(',')
-    a = []
-    for j in range(len(r)):
-        a.append(int(r[j].strip()))
-    data.append(a[:])
-
-pp = pprint.PrettyPrinter(width=180, compact=True)
-pp.pprint(data)
-
-def manhattan(item1, item2):
-    return abs(item1[0] - item2[0]) + abs(item1[1] - item2[1])
-
-# Returns [ [distance, item no in items] ]
-# sorted on distance to ever
-def manhattanArray(point, items):
-    a = []
-    for item in items:
-        if point != item:
-            a.append([manhattan(item, point),items.index(item)])
-    # print(a)
-    # return sorted(a)
-    return sorted(a, key=lambda t:t[0])
-
-
-        
-maxX = max([item[0] for item in data])
-maxY = max([item[1] for item in data])
-
-# All candidates
-candidates3 = [i for i in range(len(data))]
-
-#Create a virual chronal grid
-x = maxX+2
-y = maxY+1
-matrix = [[0] * x for i in range(y)]
-for item in data:
-    matrix[item[0]][item[1]] = str(data.index(item))
-
-#Print it!
-# print('Matrix:')
-# for row in matrix:
-#     print(' '.join([str(elem) for elem in row]))
-
-maxNum = 0
-for col in range(x):
-    for j in range(y):
-        points = manhattanArray([col,j],data)
-        point = points[0]
-        # print(points)
-        if [col,j] in data:
-            # print([col,j])
-            if DEBUG:
-                value = chr(ord('A')+data.index([col,j]))
-            else:
-                value = data.index([col,j])
-
-            if maxNum < data.index([col,j]):
-                maxNum = data.index([col,j])
-        elif (points[0][0] == points[1][0]):
-            value = '.'
-        else:
-            if DEBUG:
-                value = chr(ord('a')+point[1])
-            else:
-                value = point[1]
-
-        matrix[j][col] = value
-        # print(matrix[j])
-if DEBUG:
-    # print('Matrix:')
-    for row in matrix:
-        print(' '.join([str(elem) for elem in row]))
-
-    print('items in list:',maxNum)
-
-
-# Remove edging areas
-
-def cleanCandidate(row, col, cand, matr):
-    value = matr[row][col]
-    if DEBUG:
-        print([col,row],':',value)
-        v = ord(value)-ord('a')
-        if v in cand:
-            cand.remove(v)
-    else:
-        if value in cand:
-            cand.remove(value)
-        
-
-for col in [0,x-1]:
-    for row in range(y):
-        cleanCandidate(row, col, candidates3, matrix)
-
-for row in [0,y-1]:
-    for col in range(x):
-        cleanCandidate(row, col, candidates3, matrix)
-
-print('Candidates 3:', len(candidates3), candidates3)
-
-patches = dict()
-saveArea = 0
-for num in candidates3:
-    count = 0
-    for col in range(x):
-        for j in range(y):
-            if DEBUG:
-                if matrix[j][col].upper() == chr(ord('A')+num):
-                    count += 1
-            else:
-                if matrix[j][col] == num:
-                    count += 1
-
-    if saveArea < count:
-        saveArea = count
-
-    if DEBUG:
-        print('Found', count, 'occurrences of', chr(ord('A')+num))
-    else:
-        print('Found', count, 'occurrences of', num)
-    
-    patches[num] = count
-
-patch = sorted(patches.items(), key = lambda t:t[1], reverse=True)[0][0]
-
-pp.pprint(sorted(patches.items(), key = lambda t:t[1], reverse=True))
-
-print('\nPart 1: The largest area not infinite is', saveArea)
-
-def manahattanSum(row, col, a):
-    sum = 0
-    for item in a:
-        sum += manhattan([col, row], item)
-    return sum
-
-saveArea = 0
-count = 0
-for col in range(x):
-    for row in range(y):
-        if DEBUG:
-            if manahattanSum(row, col, data) < 32:
-                count += 1
-        else:
-            if manahattanSum(row, col, data) < 10000:
-                count += 1
-            
-
-#Print it!
-# print('Matrix:')
-# for row in matrix:
-#     print(' '.join([str(elem) for elem in row]))
-
-print('\nPart 2: The region size is', count)
+# print('\nPart 2: The region size is', count)
 
 
 end = datetime.datetime.now()
--- 2018/code/AoC10_test.py
+++ 2018/code/AoC10_test.py
@@ -5,18 +5,29 @@ from AoC10 import Point,SkyMap
 class MessageTest(unittest.TestCase):
     def test_manhattan(self):
         # arrange
-        points = [Point([0,0],[1,1]), Point([0,2],[2,2])]
+        points = [Point([[0,0],[1,1]]), Point([[0,2],[2,2]])]
 
         # act
         result = points[0].manhattan(points[1])
 
         # assert
-        self.assertEqual(result, 3)
+        self.assertEqual(result, 2)
         # self.assertEqual(result, [ 4, 2, 37 ])
+
+    def test_move_point_one_second(self):
+        # arrange
+        p = Point([[0,0],[1,1]])
+
+        # act
+        p.moveOneSecond()
+        
+        #assert
+        self.assertEqual(p, Point([[1,0],[1,1]]))
+
 
     def test_move_one_second(self):
         # arrange
-        points = [Point([0,0],[-1,1]), Point([0,2],[2,2])]
+        points = [[[0,0],[-1,1]], [[0,2],[2,2]]]
         m = SkyMap(points)
 
         # act
