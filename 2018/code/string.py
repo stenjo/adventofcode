04dcccb58949764b83dd37ed9f13e552f503115d
Sten Otto Johnsen <STEOJ@statoil.com>
2018-12-12 17:21:06 +0100

    Trying real data

--- 2018/code/string.py
+++ 2018/code/string.py
@@ -1,6 +1,7 @@
 # Testing string handling in Python
 
 initial = '#..#.#..##......###...###...........'
+initial = '##.#..#.#..#.####.#########.#...#.#.#......##.#.#...##.....#...#...#.##.#...##...#.####.##..#.#..#.'
 patternstrings = [  '...## => #',
                     '..#.. => #',
                     '.#... => #',
@@ -16,19 +17,41 @@ patternstrings = [  '...## => #',
                     '###.# => #',
                     '####. => #']
 
+patternstrings = [  '.#.#. => #',
+                    '.#... => #',
+                    '##### => #',
+                    '#..#. => #',
+                    '#...# => #',
+                    '###.# => #',
+                    '...## => #',
+                    '#.##. => #',
+                    '.#.## => #',
+                    '##.#. => #',
+                    # '...#. => .',
+                    '..### => #',
+                    '###.. => #',
+                    # '##... => .',
+                    # '..##. => .',
+                    # '.##.# => .',
+                    # '##.## => .',
+                    # '.##.. => .',
+                    '##..# => #',
+                    # '#.#.# => .',
+                    '#..## => #']
+
 patterns = [ p.split(' => ')[0] for p in patternstrings]
 print(patterns)
 offset = 5
-generationstring = '.'*offset+initial+'.....'
+generationstring = '.'*offset+initial+'............'
 count = initial.count('#')
 print('{0:2d}: {1:s}'.format(0,generationstring))
 for n in range(1,21):
     nextGenString ="."*len(initial)
-    length = len(generationstring)
+    length = len(initial)
     potsum = 0
     first = length
     maxi = 0
-    for i in range(-offset,length+offset):
+    for i in range(-offset,length+offset+10):
         # print(generationstring[i-2:i+3], generationstring[i-2:i+3] in patterns)
         if i > 1-offset and i < length + 3 + offset:
             pattern = generationstring[i-2+offset:i+3+offset]
@@ -38,11 +61,11 @@ for n in range(1,21):
                 potsum += i
                 if first > i: first = i
                 if maxi < i: maxi = i
-            # else:
-            #     nextGenString = nextGenString[:i+offset]+' '+nextGenString[i+1+offset:]
+            else:
+                nextGenString = nextGenString[:i+offset]+'.'+nextGenString[i+1+offset:]
 
 
     nextGenString += '.' * (length - len(nextGenString))
     generationstring = nextGenString[:]
-    print('{0:2d}: {1:s} - {2:d} pots, sum {3:d}, first: {4:d}, index:{5:d}'.format(n,generationstring, generationstring.count('#'), potsum, len(nextGenString), maxi))
+    print('{0:2d}: {1:s} - {2:2d} pots, sum {3:3d}, first: {4:2d}, index:{5:d}'.format(n,generationstring, generationstring.count('#'), potsum, first, maxi))
 print(count)
