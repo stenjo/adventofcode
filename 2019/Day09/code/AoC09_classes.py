4267853d63d913ac6458901448ed5ad31ab5d43a
Sten Otto Johnsen <sten.johnsen@bouvet.no>
2019-12-08 11:57:12 +0100

    Preparing Day9

---
+++ 2019/Day09/code/AoC09_classes.py
@@ -0,0 +1,12 @@
+# Advent of Code 2019: https://adventofcode.com/2019/day/9
+# 
+# 
+import math
+
+class Mapper():
+
+    def __init__(self, data, size):
+        return 0
+
+
+
---
+++ 2019/Day09/code/AoC09_test.py
@@ -0,0 +1,14 @@
+#Test file for Advent of Code 2019: https://adventofcode.com/2019/day/9
+
+import unittest
+from AoC09_classes import Mapper
+
+class RunMapTest(unittest.TestCase):
+
+    def setUp(self):
+        return super().setUp()
+
+
+
+if __name__ == '__main__':
+    unittest.main()
