e27b979aa26b50413b409a3d83de3c3f199379eb
Sten Otto Johnsen <sten.johnsen@bouvet.no>
2019-12-09 07:44:03 +0100

    Attempt on Day9

--- 2019/Day09/code/AoC09.py
+++ 2019/Day09/code/AoC09.py
@@ -3,16 +3,23 @@
 # 
 
 from pprint import pprint
-from AoC09_classes import Mapper
+from AoC09_classes import Compute
 
 infile = open('data/input_09.txt','r')
-inputData1 = infile.readline()
+inputData1 = infile.readline().strip().split(',')
 
-w = Mapper(inputData1, '25x6')
 
 # Part 1
-print("Part 1: ", w.Find1x2OnLowest0())
+w = Compute(inputData1)
+w.LoadInput([1])
+w.LoadProgram(inputData1)
+result = w.RunCompute()
+pprint(w.GetOutputs())    
+# results=c.GetOutputs()
+print(w.GetOutputs())
+
+print("Part 1: ", result)
 
 # Part 2
 print("Part 2: ")
-w.PrintImage()
+# w.PrintImage()
--- 2019/Day09/code/AoC09_classes.py
+++ 2019/Day09/code/AoC09_classes.py
@@ -6,7 +6,166 @@ import math
 class Mapper():
 
     def __init__(self, data, size):
-        return 0
+        return
+
+
+class Compute():
+
+    program = []
+    inputList = []
+    outputList = []
+    progPtr = 0
+    relBase = 0
+
+    def __init__(self, data):
+        self.LoadProgram(data)
+
+    def LoadInput(self, data):
+        self.inputList = []
+        for n in data:
+            self.inputList.append(n)
+        self.inputList.reverse()
+
+    def LoadProgram(self, data):
+        self.program = list(map(int, data))
+        self.progPtr = 0
+        self.relBase = 0
+    
+    def AddToOutput(self, data):
+        self.outputList.append(data)
+
+    def GetNextInput(self):
+        return self.inputList.pop() if len(self.inputList) > 0 else None
+
+    def ProgramFinished(self):
+        return self.program[self.progPtr] == 99
+
+    def HasOutput(self):
+        return len(self.outputList) > 0
+
+    def RunCompute(self):
+        self.progPtr = 0
+        self.outputList = []
+        while self.ProgramFinished() == False:
+            self.RunOnce()
+
+        return self.outputList[-1] if len(self.outputList) > 0 else self.program[self.progPtr]
+
+    def RunForOutput(self):
+        # self.progPtr = 0
+        self.outputList = []
+        while self.HasOutput() == False and self.ProgramFinished() == False:
+            self.RunOnce()
+
+        return self.outputList
+
+
+    def RunOnce(self):
+            modes = self.program[self.progPtr] // 100
+
+            opcode = self.program[self.progPtr] % 100
+
+            arg1 = self.program[self.progPtr+1]
+            val1 = arg1 
+            if modes % 10 == 0 and opcode != 3:
+                self.ExtendMemory(arg1)
+                val1 = self.program[arg1]
+
+            val1 = self.program[arg1 + self.relBase] if modes % 10 == 2  else val1
+
+            arg2 = self.program[self.progPtr+2] if self.progPtr+2 < len(self.program) else None
+            val2 = self.program[arg2] if modes // 10 % 10 == 0 and arg2 < len(self.program) else arg2
+            val2 = arg2 + self.relBase if modes // 10 % 10 == 2 and arg2 < len(self.program) else val2
+
+            dest = self.program[self.progPtr+3] if self.progPtr + 3 < len(self.program) else None
+
+            dispatch = {
+                1: self.OpAdd,
+                2: self.OpMul,
+                3: self.OpLoad,
+                4: self.OpSave,
+                5: self.OpJumpOnTrue,
+                6: self.OpJumpOnFalse,
+                7: self.OpLessThan,
+                8: self.OpEquals,
+                9: self.OpAdjRBase
+            }
+
+            self.progPtr = dispatch[opcode](val1, val2, dest, self.progPtr)
+
+
+    def OpAdd(self, v1, v2, d, i):
+        self.ExtendMemory(d)
+        self.program[d] = v1 + v2
+        return i + 4
+
+    def OpMul(self, v1, v2, d, i):
+        self.ExtendMemory(d)
+        self.program[d] = v1 * v2
+        return i + 4
+
+    def OpLoad(self, v1, v2, d, i):
+        self.ExtendMemory(v1)
+        self.program[v1] = self.GetNextInput()
+        return i + 2
+
+    def OpSave(self, v1, v2, d, i):
+        self.AddToOutput(v1)
+        return i + 2
+
+    def OpJumpOnTrue(self, v1, v2, d, i):
+        i = v2 if v1 != 0 else i + 3
+        self.ExtendMemory(i)
+        return i
+
+    def OpJumpOnFalse(self, v1, v2, d, i):
+        i = v2 if v1 == 0 else i + 3
+        self.ExtendMemory(i)
+        return i
+
+    def OpLessThan(self, v1, v2, d, i):
+        self.ExtendMemory(d)
+        self.program[d] = 1 if v1 < v2 else 0
+        return i + 4
+
+    def OpEquals(self, v1, v2, d, i):
+        self.ExtendMemory(d)
+        self.program[d] = 1 if v1 == v2 else 0
+        return i + 4
+
+    def OpAdjRBase(self, v1, v2, d, i):
+        self.relBase += v1
+        return i + 2
+
+    def GetValueAt(self, progPtr):
+        self.ExtendMemory(progPtr)
+        return self.program[progPtr]
+
+    def GetOutputs(self):
+        return self.outputList
+
+    def ExtendMemory(self, ptrNeed):
+        if (ptrNeed + 1) > len(self.program):
+            missing = ptrNeed + 1 - len(self.program)
+            self.program += [0 for i in range(missing)]
+
+    def Restore(self):
+        self.program[1] = 12
+        self.program[2] = 2
+        return self.program
+
+    def FindNounAndVerb(self, inputdata):
+        noun = 0
+        verb = 0
+        for noun in range(100):
+            for verb in range(100):
+                self.program = list(map(int, inputdata))
+                self.program[1] = noun
+                self.program[2] = verb
+                result = self.RunCompute()
+                if result[0] == 19690720:
+                    return 100 * noun + verb
+
 
 
 
