891f61e31aa63850fc0d7fd0b79ad247ceb83cad
Sten Otto Johnsen <STEOJ@statoil.com>
2020-12-06 15:17:53 +0100

    Playing with reducing codelines

--- 2020/Day06/Day06.jl
+++ 2020/Day06/Day06.jl
@@ -18,27 +18,37 @@ inputdata = readlines("input.txt")
 function CleanUpInput(input)
     chunk = []
     output = []
-    for item in input
-        if item == ""
-            push!(output, copy(chunk))
-            empty!(chunk)
-        else
-            push!(chunk, item)
-        end
-    end
+    foreach(i->(i != "" ? push!(chunk, i) : begin push!(output, copy(chunk)); empty!(chunk); end), input)
     push!(output, copy(chunk))
-    output
 end
+
 @testset "  CleanUpInput" begin
     @test length(CleanUpInput(testinput)) == 1
     @test length(CleanUpInput(testlines)) == 5
     @test length(CleanUpInput(testlines)[3]) == 2 
 end
 
-function AnyOneAnswers(group)
-    unique(join(group))
+function GetInput(str=read("input.txt", String))
+    map(split, split(str, "\r\n\r\n"))
 end
+@testset "GetInput" begin
+    @test length(GetInput(read("testinput.txt", String))) == 5
+    @test length(GetInput(read("testinput.txt", String))[3]) == 2 
+end
+
+function GetDefaultInput()
+    # map(split, split(read("input.txt", String), "\r\n\r\n"))
+    map(split, split(read("testinput.txt", String), "\r\n\r\n"))
+end
+@testset "GetDefaultInput" begin
+    @test length(GetDefaultInput()) == 5
+    @test length(GetDefaultInput()[3]) == 2 
+end
+
+AnyOneAnswers(group) = join(union(group...))
+
 @testset "AnyOneAnswers" begin
+    @test length(AnyOneAnswers(testinput)) == 6 #abc
     @test length(AnyOneAnswers(CleanUpInput(testlines)[1])) == 3 #abc
     @test length(AnyOneAnswers(CleanUpInput(testlines)[2])) == 3 #abc
     @test length(AnyOneAnswers(CleanUpInput(testlines)[3])) == 3 #abc
@@ -47,19 +57,10 @@ end
     @test sum(length.(AnyOneAnswers.(CleanUpInput(testlines)))) == 11
 end
 
-function EveryOneAnswers(group)
-    count(v->v==group[1], group)
-    questions = []
-    for q in join(group)
-        if count(x->(q in x), group) == length(group)
-            push!(questions, q)
-        end
-    end
-    join(unique(questions))
-end
-
+EveryOneAnswers(group) = join(intersect(group...))
 
 @testset "EveryOneAnswers" begin
+    @test length(EveryOneAnswers(testinput)) == 3 #abc
     @test length(EveryOneAnswers(CleanUpInput(testlines)[1])) == 3 #abc
     @test length(EveryOneAnswers(CleanUpInput(testlines)[2])) == 0 #abc
     @test length(EveryOneAnswers(CleanUpInput(testlines)[3])) == 1 #abc
@@ -70,18 +71,15 @@ end
 
 
 # Part 1
-function partOne(rawList)
-    sum(length.(AnyOneAnswers.(CleanUpInput(rawList))))
-end
+partOne(rawList) = sum(length.(AnyOneAnswers.(CleanUpInput(rawList))))
 
 @test partOne(testlines) == 11
 @test partOne(inputdata) == 6590
 println(string("Part one: ", partOne(inputdata)))
 
 # Part 2
-function partTwo(rawList)
-    sum(length.(EveryOneAnswers.(CleanUpInput(rawList))))
-end
+partTwo(rawList) = sum(length.(EveryOneAnswers.(CleanUpInput(rawList))))
 
-# @test partTwo(inputdata) == 739
+@test partTwo(inputdata) == 3288
 println(string("Part two: ", partTwo(inputdata)))
+println(sum(length.(map(g->join(intersect(g...)), map(split, split(read("input.txt", String), "\r\n\r\n")) ))))
