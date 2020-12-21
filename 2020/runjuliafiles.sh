7ff76e8aac024b6860e36691139f9f65064dad8a
Sten Otto Johnsen <sten.johnsen@bouvet.no>
2020-12-21 15:43:39 +0100

    Updating scripts to run

--- 2020/runjuliafiles.sh
+++ 2020/runjuliafiles.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-# /c/Users/sten.johnsen/AppData/Local/Programs/Julia\ 1.5.3/bin/julia.exe Day21.jl
-input=*/
 
 function runjulia {
     INPUT=$1
     SUBSTRING=$(echo $INPUT| cut -d'-' -f 1)
     if [[ "$SUBSTRING" =~ ^Day.* ]]; then
         cp $1* .
-        /c/Users/sten.johnsen/AppData/Local/Programs/Julia\ 1.5.3/bin/julia.exe $SUBSTRING.jl
+        julia $SUBSTRING.jl
+        # /c/Users/sten.johnsen/AppData/Local/Programs/Julia\ 1.5.3/bin/julia.exe Day21.jl
     fi
-#    for f in $1/*.jl; do FILENAME=${f%%.*};  echo ${FILENAME};  done;
 }
 
 for directory in */; do  
