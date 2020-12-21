c6b39ca0e3ca99573749951068167ed78770c51a
Sten Otto Johnsen <sten.johnsen@bouvet.no>
2020-12-21 15:30:20 +0100

    Working on bash script

---
+++ 2020/runjuliafiles.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# /c/Users/sten.johnsen/AppData/Local/Programs/Julia\ 1.5.3/bin/julia.exe Day21.jl
+input=*/
+
+function runjulia {
+    INPUT=$1
+    SUBSTRING=$(echo $INPUT| cut -d'-' -f 1)
+    if [[ $SUBSTRING == A*]];
+    then
+    #    cp $1* .
+        echo $SUBSTRING.jl
+    fi
+#    for f in $1/*.jl; do FILENAME=${f%%.*};  echo ${FILENAME};  done;
+}
+
+for directory in */; do  
+    runjulia $directory
+done;
