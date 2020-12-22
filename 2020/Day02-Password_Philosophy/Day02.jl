276a21e9b2d78e18f45f91957b66d345c5a16284
Sten Otto Johnsen <sten.johnsen@bouvet.no>
2020-12-22 14:30:39 +0100

    Fixed output and benchmark on Day02

--- 2020/Day02-Password_Philosophy/Day02.jl
+++ 2020/Day02-Password_Philosophy/Day02.jl
@@ -3,6 +3,7 @@
 # 
 using DelimitedFiles
 using Test
+using BenchmarkTools
 
 testinput = ["1-3 a: abcde","1-3 b: cdefg","2-9 c: ccccccccc"]
 
@@ -23,18 +24,21 @@ end
 # Part 1
 ValidPassWord(f::PassWord) = f.first <= count(i->(i == f.letter), f.pass) <= f.last
 
-partOne(list) = count(ValidPassWord.(PassWord.(list)))
+partOne(list=inputdata) = count(ValidPassWord.(PassWord.(list)))
 
 @test partOne(testinput) == 2   # According to the puzzle text
+@test partOne(inputdata) == 410   # According to the puzzle text
 
 println("Part one: ", partOne(inputdata))
+@time partOne()
 
 # Part 2
 ValidPassWord2(f::PassWord) = (f.pass[f.first] == f.letter && f.pass[f.last] != f.letter) || (f.pass[f.first] != f.letter && f.pass[f.last] == f.letter)
 
-partTwo(list) = count(ValidPassWord2.(PassWord.(list)))
+partTwo(list=inputdata) = count(ValidPassWord2.(PassWord.(list)))
 
 @test partTwo(testinput) == 1
+@test partTwo() == 694
 
 println("Part two: ", partTwo(inputdata))
-# end # module
+@time partTwo()
