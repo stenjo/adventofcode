16fff7efe4c951162ac56770b4fba7611cf22247
Sten Otto Johnsen <STEOJ@statoil.com>
2021-12-14 15:33:40 +0100

    Trying counting pairs

--- 2021/go/day14/day14.go
+++ 2021/go/day14/day14.go
@@ -2,22 +2,32 @@ package day14
 
 import "strings"
 
-type Polymer string
+type Polymer map[string]int
 
 func GetElementDiff(str []string) int {
 	template := str[0]
 	rules := ParseRules(str[2:])
-	elements := Polymer(template)
+	elements := ParsePolymer(template)
 	for i := 0; i < 10; i++ {
 		elements.RunStep(rules)
 	}
 	return elements.max() - elements.min()
 }
 
+func ParsePolymer(template string) Polymer {
+	polymer := Polymer{}
+
+	for i := 0; i < len(template)-1; i++ {
+		polymer[template[i:i+2]] = 1
+	}
+
+	return polymer
+}
+
 func GetElementDiff40(str []string) int {
 	template := str[0]
 	rules := ParseRules(str[2:])
-	elements := Polymer(template)
+	elements := ParsePolymer(template)
 	for i := 0; i < 40; i++ {
 		elements.RunStep(rules)
 	}
@@ -25,15 +35,13 @@ func GetElementDiff40(str []string) int {
 }
 
 func (e *Polymer) RunStep(rules map[string]string) {
-	var result Polymer
-	var last Polymer
-	for i:=0; i < len(*e)-1; i++ {
-		pair := string((*e)[i:i+2])
-		ins := rules[pair]
-		result = result + Polymer(pair[0]) + Polymer(ins) // + Polymer(pair[1]) 
-		last = Polymer(pair[1]) 
+	result := Polymer{}
+	// var last Polymer
+	for pair,val := range (*e) {
+		result[string(pair[0]) + rules[pair]] += val 
+		result[rules[pair] + string(pair[1])] += val
 	}
-	(*e) = result + last
+	(*e) = result
 }
 
 func ParseRules(str []string) map[string]string {
@@ -47,28 +55,20 @@ func ParseRules(str []string) map[string]string {
 }
 
 func (e Polymer) max() int {
-	scores := make(map[rune]int, len(e))
 	var maxVal int
-	for _, c := range e {
-		scores[c]++
-	}
-	for _, r := range scores {
-		if r > maxVal {
-			maxVal = r
+	for _,v := range e {
+		if v > maxVal {
+			maxVal = v
 		}
 	}
 	return maxVal
 }
 
 func (e Polymer) min() int {
-	scores := make(map[rune]int, len(e))
 	minVal := len(e)
-	for _, c := range e {
-		scores[c]++
-	}
-	for _, r := range scores {
-		if r < minVal {
-			minVal = r
+	for _, v := range e {
+		if v < minVal {
+			minVal = v
 		}
 	}
 	return minVal
