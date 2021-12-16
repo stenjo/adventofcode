07f248b0b96b9087ed530087b846a7e3849f413c
Sten Otto Johnsen <sten.johnsen@bouvet.no>
2021-12-16 21:54:19 +0100

    At least I tried...

--- 2021/go/day16/day16.go
+++ 2021/go/day16/day16.go
@@ -13,31 +13,143 @@ type Package struct {
 }
 var LITERAL = 4
 
-func pkgDecode(s string) []Package {
+func GetVersionsSum(s string) int {
+	pkgs,_ := pkgDecode(toBits(s))
+	var sum int
+	for _,p := range pkgs {
+		sum += p.ver
+	}
+	return sum
+}
+
+func pkgDecode(bits string) ([]Package,string) {
 	pkgs := make([]Package,0)
-	pkg, pld := decode(s)
-	pkgs = append(pkgs, pkg)
-	for pkg.typ != LITERAL {
-		ltId := getBits(pld, 0, 1)
+	pkg,rem := unpack(bits)
+	if pkg.typ != 0 {
+		pkgs = append(pkgs, pkg)
+	}
+	if pkg.typ == LITERAL {
+		return pkgs, rem
+	} else {
+		ltId := getBits(rem, 0, 1)
+		var subpkgs []Package
 		switch ltId {
 		case 0:	// length is a 15-bit number representing the number of bits in the sub-packets
-			pkgLen := getBits(pld,1,15)
-			pld = pld[15+1:]
+			pkgLen := getBits(rem,1,15)
+			pld := (rem[15+1:])[:pkgLen]
 			for len(pld) > 0 {
-				pkg, pld = decode(pld[:pkgLen])
-				pkgs = append(pkgs, pkg)
+				subpkgs, pld = pkgDecode(pld)
+				pkgs = append(pkgs, subpkgs...)
 			}
+			return pkgs, (rem[15+1:])[pkgLen:]
 
 		case 1: // length is a 11-bit number representing the number of sub-packets
-			pkgCount := getBits(pld, 1, 11)
-			pld = pld[11+1:]
+			pkgCount := getBits(rem, 1, 11)
+			pld := rem[11+1:]
 			for i := 0; i < pkgCount; i++ {
-				pkg, pld = decode(pld)
-				pkgs = append(pkgs, pkg)
+				subpkgs, pld = pkgDecode(pld)
+				pkgs = append(pkgs, subpkgs...)
 			}
+			return pkgs, pld
 		}
-	} 
-	return pkgs
+	}
+	// return pkgs, payload
+	return pkgs, ""
+}
+
+// Unwrap package and return remaining if all not used
+func unpack(pkg string) (Package, string) {
+	var payload string
+	p := Package{}
+
+	bits := pkg
+
+	if len(bits) < 11 { return p, payload }
+
+	p.ver = getBits(bits, 0,3)
+	p.typ = getBits(bits, 3,3)
+	payload = bits[6:]
+
+	if p.typ == LITERAL {
+		p.val,payload = literals(payload)
+	}
+	return p, payload
+}
+
+func toHex(bits string) string {
+	var val int
+	var hex string
+
+	pad := (4 - (len(bits) % 4)) % 4
+	bits = bits + "00000"[:pad]
+
+	for len(bits) > 0 {
+		chunk := ""
+		if len(bits) < 4 {
+			chunk = bits
+			bits = ""
+		} else {
+			chunk = bits[:4]
+			bits = bits[4:]
+		}
+		fmt.Sscanf(chunk, "%b", &val)
+		hex = hex + fmt.Sprintf("%X", val)
+	}
+	return hex
+}
+
+func toBits(hex string) string {
+	var value int
+	var first int
+	var bits string
+
+	for len(hex) > 0 {
+		chunk := ""
+		if len(hex) < 8 {
+			chunk = hex
+			hex = ""
+		} else {
+			chunk = hex[:8]
+			hex = hex[8:]
+		}
+		fmt.Sscanf(chunk, "%X", &value)
+		r := fmt.Sprintf("%b", value)
+		// Pad the bitstring
+		fmt.Sscanf(chunk[:1], "%X", &first)
+		if first < 8 { r = "0"+r}
+		if first < 4 { r = "0"+r}
+		if first < 2 { r = "0"+r}
+
+		bits = bits + r
+	
+	}
+
+	return bits
+}
+
+func literals(hex string) (int,string) {
+
+	remainder := hex
+	var value string
+	var literal int
+	
+	for done := false; !done; {
+		var end int
+		var bits string
+		remainder = hex[5:]
+		fmt.Sscanf(hex[:5], "%1b%s", &end, &bits)
+		value = value + bits
+		if end == 0 {
+			fmt.Sscanf(value, "%b", &literal)
+			return literal, remainder
+		}
+		if len(hex) < 5 {
+			done = true
+		} else {
+			hex = hex[5:]
+		}
+	}
+	return literal, hex
 }
 
 func decode(pkg string) (Package,string) {
@@ -57,21 +169,6 @@ func decode(pkg string) (Package,string) {
 	return p, payload
 }
 
-func toBits(s string) string {
-	var value int
-	var first int
-
-	fmt.Sscanf(s, "%X", &value)
-	r := fmt.Sprintf("%b", value)
-	// Pad the bitstring
-	fmt.Sscanf(s[:1], "%X", &first)
-	if first < 8 { r = "0"+r}
-	if first < 4 { r = "0"+r}
-	if first < 2 { r = "0"+r}
-
-	return r
-}
-
 func getBits(bits string, index,length int) int {
 
 	var val int
@@ -86,19 +183,19 @@ func getBits(bits string, index,length int) int {
 	return val
 }
 
-func parseLiteral(s string) int {
+func parseLiteral(hex string) int {
 	var val string
 	var literal int
-	for len(s) > 0 {
+	for len(hex) > 0 {
 		var end int
 		var bits string
-		fmt.Sscanf(s[:5], "%1b%s", &end, &bits)
+		fmt.Sscanf(hex[:5], "%1b%s", &end, &bits)
 		val = val + bits
 		if end == 0 {
 			fmt.Sscanf(val, "%b", &literal)
 			return literal
 		}
-		s = s[5:]
+		hex = hex[5:]
 	}
 	return literal
 }
--- 2021/go/day16/day16_test.go
+++ 2021/go/day16/day16_test.go
@@ -100,23 +100,112 @@ func Test_toBits(t *testing.T) {
 	}
 }
 
-func Test_pkgDecode(t *testing.T) {
+		// {"1", args{pkg: "D2FE28"}, Package{ver: 6, typ: 4, val: 2021}, "000"},
+		// {"2", args{pkg: "D2FE287"}, Package{ver: 6, typ: 4, val: 2021}, "0000111"},
+		// {"3", args{pkg: "D2FE289"}, Package{ver: 6, typ: 4, val: 2021}, "0001001"},
+
+func Test_literals(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  int
+		want1 string
+	}{
+		// TODO: Add test cases.
+		{"1", args{s: "101111111000101000"}, 2021, "000"},
+		{"2", args{s: "1011111110001010000111"}, 2021, "0000111"},
+		{"3", args{s: "1011111110001010001001"}, 2021, "0001001"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := literals(tt.args.s)
+			if got != tt.want {
+				t.Errorf("literals() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("literals() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func Test_toHex(t *testing.T) {
+	type args struct {
+		bits string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{"1", args{bits: "110100101111111000101000"}, "D2FE28"},
+		{"2", args{bits: "110100101111111000101"}, "D2FE28"},
+		{"3", args{bits: "1101001011111110001010"}, "D2FE28"},
+		{"4", args{bits: "11010010111111100010100"}, "D2FE28"},
+		{"5", args{bits: "110100101111111000101000110100101111111000101000110100101111111000101"}, "D2FE28D2FE28D2FE28"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := toHex(tt.args.bits); got != tt.want {
+				t.Errorf("toHex() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetVersionsSum(t *testing.T) {
 	type args struct {
 		s string
 	}
 	tests := []struct {
 		name string
 		args args
-		want []Package
+		want int
 	}{
-		{"1", args{s: "D2FE28"}, []Package{{ver: 6, typ: 4, val: 2021}}},
-		// {"2", args{s: "38006F45291200"}, []Package{{ver: 1, typ: 6, val: 0}}},
-		// {"3", args{s: "EE00D40C823060"}, []Package{{ver: 7, typ: 3, val: 0}}},
+		// TODO: Add test cases.
+		// {"1", args{s: "D2FE28"}, 6},
+		// {"2", args{s: "38006F45291200"}, 9},
+		// {"3", args{s: "EE00D40C823060"}, 14},
+		// {"4", args{s: "8A004A801A8002F478"}, 16},
+		{"5", args{s: "620080001611562C8802118E34"}, 12},
+		{"6", args{s: "C0015000016115A2E0802F182340"}, 23},
+		{"7", args{s: "A0016C880162017C3686B18A3D4780"}, 31},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := pkgDecode(tt.args.s); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("pkgDecode() = %v, want %v", got, tt.want)
+			if got := GetVersionsSum(tt.args.s); got != tt.want {
+				t.Errorf("GetVersionsSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_pkgDecode(t *testing.T) {
+	type args struct {
+		bits string
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  []Package
+		want1 string
+	}{
+		// {"1", args{bits: "110100101111111000101000"}, []Package{{ver: 6, typ: 4, val: 2021}}, "000"},
+		{"2", args{bits: "00111000000000000110111101000101001010010001001000000000"}, []Package{{ver: 1, typ: 6, val: 0}, {ver: 6, typ: 4, val: 10}, {ver: 2, typ: 4, val: 20}}, "0000000"},
+		{"3", args{bits: "11101110000000001101010000001100100000100011000001100000"}, []Package{{ver: 7, typ: 3, val: 0}, {ver: 2, typ: 4, val: 1}, {ver: 4, typ: 4, val: 2}, {ver: 1, typ: 4, val: 3}},"00000"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := pkgDecode(tt.args.bits)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("pkgDecode() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("pkgDecode() got1 = %v, want %v", got1, tt.want1)
 			}
 		})
 	}
