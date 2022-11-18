6d56428edc08634926416733442ec17392faac29
Sten Otto Johnsen <steoj@equinor.com>
2022-11-18 21:44:58 +0100

    create template files

--- 2022/test/template.test.ts
+++ 2022/test/template.test.ts
@@ -1,4 +1,4 @@
-import { parseCommand, navigate, navigateWithAim } from '../src/day02';
+import { parseCommand, navigate, navigateWithAim } from '../src/template';
 import * as fs from 'fs';
 import * as path from 'path';
 
