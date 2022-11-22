91d57dd81055b016dbe0ed1e7da5c6311413d6a3
Sten Johnsen <sten@thinea.no>
2022-11-22 08:25:07 +0100

    counting quoted string

---
+++ 2015/ts/test/day08.test.ts
@@ -0,0 +1,17 @@
+import { Matchsticks } from '../src/day08'
+
+const m = new Matchsticks();
+describe('Matchsticks', () => {
+    it('should count zero caracters', ()=>{
+        
+        let chars = m.CountChars('""');
+
+        expect(chars).toBe(0);
+    })
+    it('should count 3 caracters', ()=>{
+        
+        let chars = m.CountChars('"abc"');
+
+        expect(chars).toBe(3);
+    })
+})
