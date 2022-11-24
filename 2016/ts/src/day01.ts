4264f6a0bdcd1e9417e76ee1048e69d119d0c8cf
Sten Johnsen <sten@thinea.no>
2022-11-24 16:51:26 +0100

    solution day1 part 1

--- 2016/ts/src/day01.ts
+++ 2016/ts/src/day01.ts
@@ -1,12 +1,72 @@
+import * as fs from 'fs';
+import * as path from 'path';
 
+class Position {
+    x: number;
+    y: number;
+    constructor(x: number, y: number) { this.x = x; this.y = y; }
+}
 export class Taxicab {
     Blocks(instruction: string): number {
         let directionsList = instruction.split(',')
-        let distance = 0;
+        let turn = ' ';
+        let heading = 'N'
+        let p:Position = new Position(0,0)
 
         directionsList.forEach(direction => {
-            distance += parseInt(direction.trim()[1]);
+            turn = getTurn(direction.trim());
+            heading = getHeading(heading, turn);
+            let length = getLength(direction.trim());
+            p = moveTo(heading, length, p);
         })
-        return distance;
+
+        return Math.abs(p.x)+ Math.abs(p.y);
+
+        function moveTo(head: string, len: number, pos: Position): Position {
+            if (head === 'N') {
+                pos.y += len;
+            }
+            if (head === 'E') {
+                pos.x += len;
+            }
+            if (head === 'S') {
+                pos.y -= len;
+            }
+            if (head === 'W') {
+                pos.x -= len;
+            }
+            return pos
+        }
+
+        function getHeading(heading: string, turn: string): string {
+            const directions: string[] = ['N', 'E', 'S', 'W'];
+            let now = directions.indexOf(heading)
+            if (turn === 'R') {
+                now = (now + 1) % directions.length;
+            }
+            if (turn === 'L') {
+                now = (now - 1)
+                if (now === -1) {
+                    now += directions.length;
+                }
+            }
+            return directions[now];
+        }
+
+        function getLength(direction: string) {
+            let numString = direction.substring(1).trim()
+            return parseInt(numString);
+        }
+
+        function getTurn(direction: string): string {
+            return direction.charAt(0);
+        }
+    }
+
+    LoadLines():string {
+        let filename = path.join(__dirname,'../../day01.txt');
+        let lines = fs.readFileSync(filename, 'utf8').trim();
+
+        return lines;
     }
 }
--- 2016/ts/test/day01.test.ts
+++ 2016/ts/test/day01.test.ts
@@ -16,8 +16,23 @@ describe('Taxicab should', () => {
         let t = new Taxicab();
         expect(t.Blocks("R2, L3")).toBe(5)
     })
-    it.skip('return 2 for R2, R2, R2', () => {
+    it('leave you 2 blocks away for R2, R2, R2', () => {
         let t = new Taxicab();
         expect(t.Blocks("R2, R2, R2")).toBe(2)
+    })
+    it('leave you 12 blocks away for R5, L5, R5, R3', () => {
+        let t = new Taxicab();
+        expect(t.Blocks("R5, L5, R5, R3")).toBe(12)
+    })
+    it('leave you 22 blocks away for R15, L5, R5, R3', () => {
+        let t = new Taxicab();
+        expect(t.Blocks("R15, L5, R5, R3")).toBe(22)
+    })
+})
+
+describe('Part 1 should', () => {
+    it('give correct answer', () => {
+        let t = new Taxicab();
+        expect(t.Blocks(t.LoadLines())).toBe(307)
     })
 })
