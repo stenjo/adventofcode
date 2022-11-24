44bb3e6f7b3588581fd47e5950597ea2d30ca42c
Sten Johnsen <sten@thinea.no>
2022-11-24 16:59:12 +0100

    r

--- 2016/ts/src/day01.ts
+++ 2016/ts/src/day01.ts
@@ -1,12 +1,39 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+// --- Day 1: No Time for a Taxicab ---
+// You're airdropped near Easter Bunny Headquarters in a city somewhere.
+// "Near", unfortunately, is as close as you can get - the instructions on the
+// Easter Bunny Recruiting Document the Elves intercepted start here, and
+// nobody had time to work them out further.
+
+// The Document indicates that you should start at the given coordinates
+// (where you just landed) and face North. Then, follow the provided sequence:
+// either turn left (L) or right (R) 90 degrees, then walk forward the given
+// number of blocks, ending at a new intersection.
+
+// There's no time to follow such ridiculous instructions on foot, though, so
+// you take a moment and work out the destination. Given that you can only
+// walk on the street grid of the city, how far is the shortest path to the
+// destination?
+
+// For example:
+
+// - Following R2, L3 leaves you 2 blocks East and 3 blocks North, or 5
+//   blocks away.
+// - R2, R2, R2 leaves you 2 blocks due South of your starting position,
+//   which is 2 blocks away.
+// - R5, L5, R5, R3 leaves you 12 blocks away.
+// How many blocks away is Easter Bunny HQ?
+
+
 class Position {
     x: number;
     y: number;
     constructor(x: number, y: number) { this.x = x; this.y = y; }
 }
 export class Taxicab {
+    
     Blocks(instruction: string): number {
         let directionsList = instruction.split(',')
         let turn = ' ';
@@ -20,7 +47,7 @@ export class Taxicab {
             p = moveTo(heading, length, p);
         })
 
-        return Math.abs(p.x)+ Math.abs(p.y);
+        return this.Manhattan(p);
 
         function moveTo(head: string, len: number, pos: Position): Position {
             if (head === 'N') {
@@ -61,6 +88,10 @@ export class Taxicab {
         function getTurn(direction: string): string {
             return direction.charAt(0);
         }
+    }
+
+    private Manhattan(p: Position): number {
+        return Math.abs(p.x) + Math.abs(p.y);
     }
 
     LoadLines():string {
