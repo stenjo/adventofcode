6301f5523b059e105a0cd9cd07f43ea9484ef89b
Sten Johnsen <sten@thinea.no>
2022-12-02 05:45:16 +0100

    part 2 day4 2017

--- 2017/ts/src/day04.ts
+++ 2017/ts/src/day04.ts
@@ -3,6 +3,45 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 export class Passphrase {
+    countValidPart2Phrases(): any {
+        let count = 0;
+        this.lines.forEach((line) => {
+            if (this.isValid(line.trim()) && !this.hasAnagram(line)) {
+                count++;
+            }
+        })
+
+        return count;
+    }
+    hasAnagram(passPhrase: string): boolean {
+
+        let parts = passPhrase.split(' ');
+        for (let i = 0; i < parts.length-1; i++) {
+            for (let j = i+1; j < parts.length; j++) {
+                if (this.isAnagram(parts[i], parts[j])) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    isAnagram(first: string, second: string): boolean {
+        let x = getSortedString(first);
+        let y = getSortedString(second);
+
+        return x == y;
+
+        function getSortedString(s:string):string {
+            return s.split('').sort((a, b) => {
+                if (a > b)
+                    return -1;
+                if (a < b)
+                    return 1;
+                return 0;
+            }).join('');
+        }
+    }
+ 
     countValidPhrases(): any {
         let count = 0;
         this.lines.forEach((line) => {
@@ -14,8 +53,8 @@ export class Passphrase {
         return count;
     }
 
-    isValid(arg0: string): any {
-        let parts = arg0.split(' ');
+    isValid(passPhrase: string): any {
+        let parts = passPhrase.split(' ');
         for (let i = 0; i < parts.length-1; i++) {
             if (parts.indexOf(parts[i], i+1) !== -1) {
                 return false;
--- 2017/ts/test/day04.test.ts
+++ 2017/ts/test/day04.test.ts
@@ -23,4 +23,35 @@ describe('Passphrase should', () => {
 
         expect(p.countValidPhrases()).toBe(325);
     })
+    it('count valid Part 2 passphrases', () => {
+        let p = new Passphrase('../../data/input4.txt')
+
+        expect(p.countValidPart2Phrases()).toBe(119);
+    })
+
+    it('detect anagram', () => {
+        let p = new Passphrase('../../data/input4.txt')
+
+        expect(p.isAnagram('iooo', 'oioo')).toBe(true);
+        expect(p.isAnagram('abcde', 'ecdab')).toBe(true);
+    })
+    it('not detect anagram', () => {
+        let p = new Passphrase('../../data/input4.txt')
+
+        expect(p.isAnagram('iiii','oiii')).toBe(false);
+    })
+
+    it('detect anagram in passphrase', () => {
+        let p = new Passphrase('../../data/input4.txt')
+
+        expect(p.hasAnagram('abcde xyz ecdab')).toBe(true);
+        expect(p.hasAnagram('oiii ioii iioi iiio')).toBe(true);
+    })
+    it('detect no anagram in passphrase', () => {
+        let p = new Passphrase('../../data/input4.txt')
+
+        expect(p.hasAnagram('abcde fghij')).toBe(false);
+        expect(p.hasAnagram('a ab abc abd abf abj')).toBe(false);
+        expect(p.hasAnagram('iiii oiii ooii oooi oooo')).toBe(false);
+    })
 })
