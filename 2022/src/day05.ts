c0169d8c29bb202d920f3050806e7c1dab8f34d1
Sten Johnsen <sten@thinea.no>
2022-12-05 08:38:06 +0100

    day05 part 1

---
+++ 2022/src/day05.ts
@@ -0,0 +1,123 @@
+
+
+export class CrateStack {
+    RemoveCrate():string {
+        return this.stack.pop() as string;
+    }
+
+    Top(): string {
+        return this.stack[this.stack.length - 1];
+    }
+
+    AddCrate(crate: string) {
+        this.stack.push(crate);
+    }
+
+    stack: string[];
+    constructor(crateStrings: string[]) {
+
+        this.stack = [];
+    }
+}
+
+export class CrateMover {
+    RunCrane(arg0: string[]) {
+        arg0.forEach(c => {
+            this.DoMove(c)
+        })
+    }
+    TopString(): string {
+        let topString = ''
+        this.crateStacks.forEach((crate) => topString += crate.Top());
+        return topString;
+    }
+    DoMove(arg0: string) {
+        if (arg0.trim().length == 0) return
+        let [_move, numOfItems, _from, fromStack, _to, toStack] = arg0.split(' ')
+
+        let items = parseInt(numOfItems)
+        let from = parseInt(fromStack)
+        let to = parseInt(toStack)
+        for (let i = 0; i < items; i++) {
+            this.crateStacks[to-1].AddCrate(this.crateStacks[from-1].RemoveCrate())
+        }
+        
+    }
+    Top(stackNumber: number): any {
+        return this.crateStacks[stackNumber-1].Top();
+    }
+    GetCrates(input: string): string[] {
+
+        let commaString = input.split('')
+        for (let i = 3; i < commaString.length; i+=4) {
+            commaString[i] = ','
+        }
+        for (let i = commaString.length-1; i >= 0; i -=2) {
+            commaString.splice(i,1)
+        }
+        return commaString.join('').split(',')
+    }
+    StackCount(): any {
+        return this.crateStacks.length;
+    }
+    
+    crateStacks: CrateStack[];
+
+    constructor(crates: string[]) {
+        this.crateStacks = [];
+        if (crates.length == 0) return
+
+        let inputStrings = crates.reverse()
+        let stacks = this.getStackRangeFromInput(inputStrings[0]);
+        for (let i = 0; i < stacks; i++) {
+            this.crateStacks.push(new CrateStack([]))
+        }
+
+        inputStrings.splice(0,1)
+
+        inputStrings.forEach(line => {
+            let crates = this.GetCrates(line)
+            for (let i = 0; i < this.StackCount(); i++) {
+                if (crates[i] != ' ') {
+                    this.crateStacks[i].AddCrate(crates[i])
+                }
+            }
+        })
+        
+    }
+
+    private getStackRangeFromInput(input: string): number {
+        let splitString = input.split(' ');
+        let filteredString = splitString.filter((item) => item != '');
+        let numbers = filteredString.map(number => parseInt(number, 10));
+        let sorted = numbers.sort();
+        return sorted.pop() as number;
+    }
+}
+
+import * as fs from 'fs';
+import * as path from 'path';
+export class FileInput {
+    Commands() {
+        let commands: string[] = [];
+        let belowStacks = false;
+        for (let i = 0; i < this.lines.length; i++)  {
+            if (belowStacks) commands.push(this.lines[i])
+            if (this.lines[i] == '') belowStacks = true;
+        }
+        return commands;
+    }
+    Stacks():string[] {
+        let stacks: string[] = [];
+        for (let i = 0; i < this.lines.length; i++)  {
+            if (this.lines[i] == '') return stacks
+            stacks.push(this.lines[i])
+        }
+        return stacks;
+    }
+    lines: string[];
+    constructor(fname: string) {
+        let filename = path.join(__dirname, fname);
+        this.lines = fs.readFileSync(filename, 'utf8').split('\n');
+    }
+}
---
+++ 2022/test/day05.test.ts
@@ -0,0 +1,136 @@
+import {CrateStack, CrateMover, FileInput} from '../src/day05'
+
+describe('CrateStack should', () => {
+    it('have test runnings', () => {
+        let cs = new CrateStack([])
+        expect(cs).not.toBeNull();
+    })
+    it('add crate to stack', () => {
+        let cs = new CrateStack([])
+        cs.AddCrate('D')
+
+        expect(cs.Top()).toBe('D');
+    })
+    it('remove crate from stack', () => {
+        let cs = new CrateStack([])
+        cs.AddCrate('D')
+        cs.AddCrate('B')
+
+        let c = cs.RemoveCrate()
+
+        expect(cs.Top()).toBe('D');
+        expect(c).toBe('B');
+    })
+})
+
+describe('CrateMover should', () => {
+    it('have test runnings', () => {
+        let cm = new CrateMover([]);
+
+        expect(cm).not.toBeNull();
+    })
+    it('get crates from string [Z] [M] [P]', () => {
+        let cm = new CrateMover([]);
+
+        let c = cm.GetCrates('[Z] [M] [P]')
+
+        expect(c).toStrictEqual(['Z','M','P']);
+    })
+    it('get crates from string     [D]    ', () => {
+        let cm = new CrateMover([]);
+
+        let c = cm.GetCrates('    [D]    ')
+
+        expect(c).toStrictEqual([' ','D',' ']);
+    })
+    it('create initial stacks', () => {
+        const crateList = [
+            '    [D]    ',
+            '[N] [C]    ',
+            '[Z] [M] [P]',
+            ' 1   2   3 '
+        ]
+        let cm = new CrateMover(crateList);
+
+        expect(cm.StackCount()).toBe(3);
+        expect(cm.Top(2)).toBe('D')
+        expect(cm.Top(3)).toBe('P')
+    })
+    it('move 1 from 2 to 1', () => {
+        const crateList = [
+            '    [D]    ',
+            '[N] [C]    ',
+            '[Z] [M] [P]',
+            ' 1   2   3 '
+        ]
+        let cm = new CrateMover(crateList);
+        cm.DoMove('move 1 from 2 to 1')
+
+        expect(cm.Top(1)).toBe('D')
+    })
+    it('move 3 from 1 to 3', () => {
+        const crateList = [
+            '    [D]    ',
+            '[N] [C]    ',
+            '[Z] [M] [P]',
+            ' 1   2   3 '
+        ]
+        let cm = new CrateMover(crateList);
+        cm.DoMove('move 1 from 2 to 1')
+        cm.DoMove('move 3 from 1 to 3')
+
+        expect(cm.Top(3)).toBe('Z')
+    })
+    it('give top string CMZ', () => {
+        const crateList = [
+            '    [D]    ',
+            '[N] [C]    ',
+            '[Z] [M] [P]',
+            ' 1   2   3 '
+        ]
+        let cm = new CrateMover(crateList);
+        cm.DoMove('move 1 from 2 to 1')
+        cm.DoMove('move 3 from 1 to 3')
+        cm.DoMove('move 2 from 2 to 1')
+        cm.DoMove('move 1 from 1 to 2')
+
+        expect(cm.TopString()).toBe('CMZ')
+    })
+
+    it('create initial stacks from input', () => {
+        let inputData = new FileInput('../test/input/day05.txt')
+        let cm = new CrateMover(inputData.Stacks());
+
+        expect(cm.StackCount()).toBe(3);
+        expect(cm.Top(2)).toBe('D')
+        expect(cm.Top(3)).toBe('P')
+    })
+
+    it('parse test input file', () => {
+        let inputData = new FileInput('../test/input/day05.txt')
+        let cm = new CrateMover(inputData.Stacks());
+        cm.RunCrane(inputData.Commands())
+
+        expect(cm.TopString()).toBe('CMZ')
+    })
+    it('parse real input file', () => {
+        let inputData = new FileInput('../input/day05.txt')
+        let cm = new CrateMover(inputData.Stacks());
+        cm.RunCrane(inputData.Commands())
+
+        expect(cm.TopString()).toBe('FWNSHLDNZ')
+    })
+})
+
+describe('FileInput should', () => {
+    it('give stacks part', () => {
+        let inputData = new FileInput('../test/input/day05.txt')
+        
+        expect(inputData.Stacks().length).toBe(4)
+    })
+    it('give commands part', () => {
+        let inputData = new FileInput('../test/input/day05.txt')
+        
+        expect(inputData.Commands().length).toBe(4)
+    })
+})
