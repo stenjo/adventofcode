7706953b62703110122b508bc699a7a1691bd4a0
Sten Johnsen <sten@thinea.no>
2022-12-06 07:44:52 +0100

    refactor

--- 2022/src/day06.ts
+++ 2022/src/day06.ts
@@ -1,36 +1,20 @@
 // --- Day 6: Tuning Trouble ---
 
 export class Communicator {
-    FindMessageMarker(dataStream: string) {
-        let stream = dataStream.split('');
-        for (let i = 14; i < stream.length; i++) {
-            let marker = stream.slice(i-14, i);
-
-            if(this.startOfMessage(marker)) {
-                return i;
-            }
-
-        }
-    }
-    FindPacketMarker(dataStream: string) {
+    FindMarker(dataStream: string, len: number):number {
         let stream = dataStream.split('');
         for (let i = 4; i < stream.length; i++) {
-            let marker = stream.slice(i-4, i);
-
-            if(this.startOfPacket(marker)) {
+            if (this.isValidMarker(stream.slice(i-len, i), len)) {
                 return i;
             }
-
         }
+        return dataStream.length
     }
 
-    startOfPacket(marker: string[]) {
-        return marker.filter((s, n, arr) => !arr.slice(0, n).includes(s)).length == 4;
+    isValidMarker(marker: string[], len: number): boolean {
+        return marker.filter((s, n, arr) => !arr.slice(0, n).includes(s)).length == len;
     }
 
-    startOfMessage(marker: string[]) {
-        return marker.filter((s, n, arr) => !arr.slice(0, n).includes(s)).length == 14;
-    }
     constructor() {
 
     }
@@ -38,11 +22,13 @@ export class Communicator {
 
 import * as fs from 'fs';
 import * as path from 'path';
+
 export class FileInput {
-    lines: string;
+
+    data: string;
 
     constructor(fname: string) {
         let file = path.join(__dirname,fname);
-        this.lines = fs.readFileSync(file, 'utf8').trim()
+        this.data = fs.readFileSync(file, 'utf8').trim()
     }
 }
--- 2022/test/day06.test.ts
+++ 2022/test/day06.test.ts
@@ -9,55 +9,55 @@ describe('Communicator should', () => {
 
     it('find start of packet marker at 5 ', () => {
         let a = new Communicator();
-        let start = a.FindPacketMarker('bvwbjplbgvbhsrlpgdmjqwftvncz')
+        let start = a.FindMarker('bvwbjplbgvbhsrlpgdmjqwftvncz', 4)
 
         expect(start).toBe(5);
     })
     it('identify start of packet marker to be false ', () => {
         let a = new Communicator();
-        let valid = a.startOfPacket('bvwb'.split(''))
+        let valid = a.isValidMarker('bvwb'.split(''), 4)
 
         expect(valid).toBe(false);
     })
     it('identify start of packet marker to be true ', () => {
         let a = new Communicator();
-        let valid = a.startOfPacket('vwbj'.split(''))
+        let valid = a.isValidMarker('vwbj'.split(''), 4)
 
         expect(valid).toBe(true);
     })
     it('find start of packet marker at 7 ', () => {
         let a = new Communicator();
-        let start = a.FindPacketMarker('mjqjpqmgbljsphdztnvjfqwrcgsmlb')
+        let start = a.FindMarker('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 4)
 
         expect(start).toBe(7);
     })
     it('find start of packet marker at 6 ', () => {
         let a = new Communicator();
-        let start = a.FindPacketMarker('nppdvjthqldpwncqszvftbrmjlhg')
+        let start = a.FindMarker('nppdvjthqldpwncqszvftbrmjlhg', 4)
 
         expect(start).toBe(6);
     })
     it('find start of packet marker for real data ', () => {
         let a = new Communicator();
-        let data = new FileInput('../input/day06.txt').lines;
+        let data = new FileInput('../input/day06.txt').data;
         
-        let start = a.FindPacketMarker(data)
+        let start = a.FindMarker(data, 4)
 
         expect(start).toBe(1909);
     })
 
     it('find start of message marker at 19 ', () => {
         let a = new Communicator();
-        let start = a.FindMessageMarker('mjqjpqmgbljsphdztnvjfqwrcgsmlb')
+        let start = a.FindMarker('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 14)
 
         expect(start).toBe(19);
     })
 
     it('find start of message marker for real data ', () => {
         let a = new Communicator();
-        let data = new FileInput('../input/day06.txt').lines;
+        let data = new FileInput('../input/day06.txt').data;
         
-        let start = a.FindMessageMarker(data)
+        let start = a.FindMarker(data, 14)
 
         expect(start).toBe(3380);
     })
