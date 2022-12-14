a81de07721976359ea086a470b2ba5acbea65478
Sten Otto Johnsen <steoj@equinor.com>
2022-12-14 06:33:02 +0100

    wip

--- 2022/src/day14.ts
+++ 2022/src/day14.ts
@@ -4,27 +4,35 @@ import * as fs from 'fs';
 import * as path from 'path';
 export class LoadLines {
     lines: string[];
-    pairs: string[][];
     constructor(fname: string) {
       this.lines = fs
         .readFileSync(path.join(__dirname, fname), 'utf8')
         .trim()
         .split('\n')
         .map(line => line.trim());
-        this.pairs = []
-
-        let pair:string[] = []
-        this.lines.forEach(line => {
-            if (line == '' && pair.length > 0) {
-                this.pairs.push(pair)
-                pair = []
-            }
-            else {
-                pair.push(line)
-            }
-        })
-        if (pair.length > 0) {
-            this.pairs.push(pair)
-        }
     }
+}
+class Rock {
+    x: number;
+    y: number;
+    constructor(x: number, y: number) { 
+        this.x = x;
+        this.y = y;
+    }
+}
+export class Cave {
+    structures: Rock[];
+
+    constructor(input: string[] = ['']) {
+        this.structures = []
+        input.forEach(line => {
+            let points:string[] = line.split(' -> ');
+            let pts:number[][] = line.split(' -> ').map(point => point.split(',').map(n=>Number(n)))
+            points.forEach(point => {
+                let [x,y] = point.split(',').map(n=>Number(n));
+                this.structures.push(new Rock(x,y));
+            })
+        })
+    }
+
 }
--- 2022/test/day14.test.ts
+++ 2022/test/day14.test.ts
@@ -1,9 +1,29 @@
-import { LoadLines } from '../src/day14'
+import { Cave, LoadLines } from '../src/day14'
 
 describe('LoadLines should', () => {
     it('load all lines', () => {
         let l = new LoadLines('../test/input/day14.txt');
 
         expect(l.lines.length).toBeGreaterThan(0)
+    })
+})
+
+describe('Cave should', () => {
+    it('not be null', () => {
+        let c = new Cave()
+
+        expect(c).not.toBeNull()
+    })
+
+    it('initialize', () => {
+        let input = new LoadLines('../test/input/day14.txt').lines
+        let c = new Cave(input)
+
+        expect(c.structures.length).toBeGreaterThan(0)
+    })
+    it('draw one line of rock', () => {
+        let c = new Cave(['498,4 -> 498,6'])
+
+        expect(c.structures.length).toBe(3)
     })
 })
