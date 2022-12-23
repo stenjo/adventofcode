e4566388e43e680ed538dcaaec2de98446a6482f
Sten Otto Johnsen <steoj@equinor.com>
2022-12-23 06:33:50 +0100

    leaving day 21

--- 2022/src/day22.ts
+++ 2022/src/day22.ts
@@ -21,26 +21,25 @@ export enum Facing {
 export class MonkeyMap {
     Move(pos: number, rotate: string) {
         if (this.facing == Facing.Down) {
-            let wallRow = this.map.map(r => r.charAt(this.column-1)).join().indexOf('#', this.row - 1) + 1;
+            let wallRow = this.getDownWallRow();
             this.row += Math.min(pos, wallRow-pos-1)
         }
         if (this.facing == Facing.Right) {
-            let walColumn = this.map[this.row-1].indexOf('#', this.column-1)+1
+            let walColumn = this.getRightWallColumn()
             if (walColumn > 0) {
-                this.column += Math.min(pos, walColumn-this.column-1)
-                this.facing = (rotate == 'R' ? this.facing + 1 : this.facing + 3) % 4 
+                this.column = this.column + Math.min(pos, walColumn-this.column-1)
+                this.updateDirection(rotate); 
                 return
             }
             if (this.column + pos > this.map[this.row-1].length) {
                 walColumn = this.map[this.row-1].indexOf('#')+1
                 if (walColumn > 0) {
-                    let remaining = this.map[this.row-1].length - this.column + 1
-                    remaining = remaining % this.map[this.row-1].length
+                    let remaining = this.getRightRowRemaining();
                     this.column = Math.min(pos - remaining + 1, walColumn-1)
                 }
             }
         }
-        this.facing = (rotate == 'R' ? this.facing + 1 : this.facing + 3) % 4 
+        this.updateDirection(rotate);  
     }
 
     column: number;
@@ -58,6 +57,24 @@ export class MonkeyMap {
         this.column = this.map[0].indexOf('.') + 1
         this.facing = Facing.Right
 
+    }
+
+    private getRightRowRemaining() {
+        let remaining = this.map[this.row - 1].length - this.column + 1;
+        remaining = remaining % this.map[this.row - 1].length;
+        return remaining;
+    }
+
+    private getDownWallRow() {
+        return this.map.map(r => r.charAt(this.column - 1)).join().indexOf('#', this.row - 1) + 1;
+    }
+
+    private getRightWallColumn() {
+        return this.map[this.row - 1].indexOf('#', this.column - 1) + 1;
+    }
+
+    private updateDirection(rotate: string) {
+        this.facing = (rotate == 'R' ? this.facing + 1 : this.facing + 3) % 4;
     }
 
     Print() {
