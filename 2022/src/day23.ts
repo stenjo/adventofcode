edb8da6e9ff472aa442d8805349359ecd30e972c
Sten Otto Johnsen <steoj@equinor.com>
2022-12-24 07:22:11 +0100

    Day 23

--- 2022/src/day23.ts
+++ 2022/src/day23.ts
@@ -34,28 +34,38 @@ class Pos {
 }
 
 export class Elf extends Pos {
-    Move() {
+    Move():number {
 
-        if (this.direction == Direction.North) {
+        if (this.proposedDirection == Direction.None) {
+            return 0;
+        }
+
+        if (this.proposedDirection == Direction.North) {
             this.y -= 1
         }
-        if (this.direction == Direction.South) {
+        if (this.proposedDirection == Direction.South) {
             this.y += 1
         }
-        if (this.direction == Direction.West) {
+        if (this.proposedDirection == Direction.West) {
             this.x -= 1
         }
-        if (this.direction == Direction.East) {
+        if (this.proposedDirection == Direction.East) {
             this.x += 1
         }
-
-        this.direction += 1
-        this.direction %= 4
+        this.proposedDirection = Direction.None
+        return 1
     }
     direction: Direction
+    proposedDirection!: Direction;
     constructor(x: number, y: number) {
         super(x, y);
         this.direction = Direction.North
+        this.proposedDirection = Direction.None
+    }
+
+    SetNextDirection() {
+        this.direction += 1;
+        this.direction %= 4;
     }
 }
 
@@ -68,11 +78,24 @@ export class Proposal extends Pos {
 }
 
 export class Grove {
+    Spaces(): number {
+        let minX = this.elves.map(c=> c.x).sort((a, b) => b - a).pop() as number
+        let minY = this.elves.map(c=> c.y).sort((a, b) => b - a).pop() as number
+        let maxX = this.elves.map(c=> c.x).sort((a, b) => a - b).pop() as number
+        let maxY = this.elves.map(c=> c.y).sort((a, b) => a - b).pop() as number
+
+        return (maxX-minX+1)*(maxY-minY+1) - this.elves.length
+
+    }
     MoveAll() {
+        let moves = 0
         this.elves.forEach(e => {
-            if (this.ProposesAt(e.x, e.y, e.direction) == 1)
-                e.Move();
+            if (this.ProposesAt(e.x, e.y, e.proposedDirection) == 1)
+                moves += e.Move();
+
+            e.SetNextDirection()
         });
+        return moves;
     }
     ProposesAt(x: number, y: number, dir:Direction = Direction.None): number {
         if (dir == Direction.North) {
@@ -94,22 +117,68 @@ export class Grove {
         return 0
     }
     Propose() {
+        this.proposes = [];
         this.elves.forEach(e => {
 
-            let pX = e.x
-            let pY = e.y - 1
+            if (!this.isElvesAround(e.x, e.y)) return
 
-            if (!this.IsElfAt(pX, pY)) {
-                this.addProposal(pX, pY);
-                return
-            }
-            
-            pY = e.y + 1
-            if (!this.IsElfAt(pX, pY)) {
-                this.addProposal(pX, pY);
-                return
+            let dir = e.direction
+            let pX = this.proposeX(e.x, dir)
+            let pY = this.proposeY(e.y, dir)
+
+            for (let i = 0; i < 4; i++) {
+                if (!this.isAnyElvesInDirection(e.x, e.y, dir)) {
+                    this.addProposal(pX, pY);
+                    e.proposedDirection = dir;
+                    return
+                }
+                dir = (dir + 1) % 4
+                pX = this.proposeX(e.x, dir)
+                pY = this.proposeY(e.y, dir)
             }
         });
+    }
+    isElvesAround(x: number, y: number) {
+        return this.IsElfAt(x-1, y-1)   || this.IsElfAt(x, y-1) || this.IsElfAt(x+1, y-1)
+            || this.IsElfAt(x-1, y)     || this.IsElfAt(x+1,y)
+            || this.IsElfAt(x-1, y+1)   || this.IsElfAt(x, y+1) || this.IsElfAt(x+1, y+1)
+    }
+    private isAnyElvesInDirection(x:number, y:number, dir: Direction):boolean {
+        if (dir == Direction.North) {
+            y -= 1
+            return this.IsElfAt(x-1, y) || this.IsElfAt(x, y) || this.IsElfAt(x+1, y)
+        }
+        if (dir == Direction.South) {
+            y += 1
+            return this.IsElfAt(x-1, y) || this.IsElfAt(x, y) || this.IsElfAt(x+1, y)
+        }
+        if (dir == Direction.West) {
+            x -= 1
+            return this.IsElfAt(x, y-1) || this.IsElfAt(x, y) || this.IsElfAt(x, y+1)
+        }
+        if (dir == Direction.East) {
+            x += 1
+            return this.IsElfAt(x, y-1) || this.IsElfAt(x, y) || this.IsElfAt(x, y+1)
+        }
+        return false
+    }
+    private proposeX(x: number, direction: Direction) {
+        if (direction == Direction.West) {
+            return x - 1
+        }
+        if (direction == Direction.East) {
+            return x + 1
+        }
+        return x
+    }
+    private proposeY(y: number, direction: Direction) {
+        if (direction == Direction.North) {
+            return y - 1
+        }
+        if (direction == Direction.South) {
+            return y + 1
+        }
+        return y
     }
     private addProposal(x: number, y: number) {
 
--- 2022/test/day23.test.ts
+++ 2022/test/day23.test.ts
@@ -70,7 +70,7 @@ describe('Grove should', () => {
 
         g.Propose()
 
-        expect(g.ProposesAt(2,3)).toBeGreaterThan(0)
+        // expect(g.ProposesAt(2,3)).toBeGreaterThan(0)
         expect(g.ProposesAt(3,5)).toBeGreaterThan(0)
     })
     it('propose 2 new positions, one north and one south', () => {
@@ -107,5 +107,166 @@ describe('Grove should', () => {
         expect(g.IsElfAt(2,2)).toBe(true)
         expect(g.IsElfAt(3,3)).toBe(true)
         expect(g.IsElfAt(2,4)).toBe(true)
+    })
+    it('move all elves according to propsed two rounds', () => {
+        const elves = [
+            '.....',
+            '..##.',
+            '..#..',
+            '.....',
+            '..##.',
+            '.....'
+        ]
+        let g = new Grove(elves);
+
+        g.Propose()
+        g.MoveAll()
+        g.Propose()
+        g.MoveAll()
+
+        expect(g.IsElfAt(2,1)).toBe(true)
+        expect(g.IsElfAt(3,1)).toBe(true)
+        expect(g.IsElfAt(1,2)).toBe(true)
+        expect(g.IsElfAt(4,3)).toBe(true)
+        expect(g.IsElfAt(2,5)).toBe(true)
+    })
+    it('move all elves according to propsed three rounds', () => {
+        const elves = [
+            '.....',
+            '..##.',
+            '..#..',
+            '.....',
+            '..##.',
+            '.....'
+        ]
+        let g = new Grove(elves);
+
+        g.Propose()
+        g.MoveAll()
+        g.Propose()
+        g.MoveAll()
+        g.Propose()
+        g.MoveAll()
+
+        expect(g.IsElfAt(2,0)).toBe(true)
+        expect(g.IsElfAt(4,1)).toBe(true)
+        expect(g.IsElfAt(0,2)).toBe(true)
+        expect(g.IsElfAt(4,3)).toBe(true)
+        expect(g.IsElfAt(2,5)).toBe(true)
+    })
+    it('move all elves according to propsed four rounds', () => {
+        const elves = [
+            '.....',
+            '..##.',
+            '..#..',
+            '.....',
+            '..##.',
+            '.....'
+        ]
+        let g = new Grove(elves);
+        let moves = 0
+
+        for (let i = 0; i < 4; i++) {
+            g.Propose()
+            moves = g.MoveAll()
+        }
+
+        expect(moves).toBe(0)
+        expect(g.Spaces()).toBe(25)
+    })
+    it('move all elves according to propsed 10 rounds', () => {
+        const elves = [
+            '..............',
+            '..............',
+            '.......#......',
+            '.....###.#....',
+            '...#...#.#....',
+            '....#...##....',
+            '...#.###......',
+            '...##.#.##....',
+            '....#..#......',
+            '..............',
+            '..............',
+            '..............',
+        ]
+        let g = new Grove(elves);
+        let moves = 0
+
+        for (let i = 0; i < 10; i++) {
+            g.Propose()
+            moves = g.MoveAll()
+        }
+
+        expect(g.Spaces()).toBe(110)
+    })
+    it('move all elves according to propsed 10 rounds with test data', () => {
+        const elves = new LoadLines('../test/input/day23.txt').lines
+        let g = new Grove(elves);
+        let moves = 0
+
+        for (let i = 0; i < 10; i++) {
+            g.Propose()
+            moves = g.MoveAll()
+        }
+
+        expect(g.Spaces()).toBe(110)
+    })
+    it('move all elves according to propsed 10 rounds with live data', () => {
+        const elves = new LoadLines('../input/day23.txt').lines
+        let g = new Grove(elves);
+        let moves = 0
+
+        for (let i = 0; i < 10; i++) {
+            g.Propose()
+            moves = g.MoveAll()
+        }
+
+        expect(g.Spaces()).toBe(4254)
+    })
+    it('move all elves according to propsed 10 rounds with test data', () => {
+        const elves = [
+            '.....',
+            '..##.',
+            '..#..',
+            '.....',
+            '..##.',
+            '.....'
+        ]
+        let g = new Grove(elves);
+        let moves = 1
+        let rounds = 0
+        for (rounds = 0; rounds < 1000 && moves != 0; rounds++) {
+            g.Propose()
+            moves = g.MoveAll()
+        }
+
+        expect(rounds).toBe(4)
+        expect(g.Spaces()).toBe(25)
+    })
+    it('move all elves according to propsed 10 rounds with test data', () => {
+        const elves = new LoadLines('../test/input/day23.txt').lines
+        let g = new Grove(elves);
+        let moves = 1
+        let rounds = 0
+        for (rounds = 0; rounds < 1000 && moves != 0; rounds++) {
+            g.Propose()
+            moves = g.MoveAll()
+        }
+
+        expect(rounds).toBe(20)
+        // expect(g.Spaces()).toBe(110)
+    })
+    it.skip('move all elves according to propsed 10 rounds with test data', () => {
+        const elves = new LoadLines('../input/day23.txt').lines
+        let g = new Grove(elves);
+        let moves = 1
+        let rounds = 0
+        for (rounds = 0; rounds < 10000 && moves != 0; rounds++) {
+            g.Propose()
+            moves = g.MoveAll()
+        }
+
+        expect(rounds).toBe(992)
+        // expect(g.Spaces()).toBe(110)
     })
 })
