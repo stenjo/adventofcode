0818d80a0a7d1b8ea2d69c213186727ba7c7b362
Sten Johnsen <sten.johnsen@bouvet.no>
2023-10-01 21:23:31 +0200

    Day03

--- 2017/python/Day03/spiral.py
+++ 2017/python/Day03/spiral.py
@@ -3,7 +3,36 @@
 # 
 
 def steps_carried(square):
-    if square == 1:
-        return 0
-    if square == 12:
-        return 3
+    x = y = dx = 0
+    dy = -1
+    step = 0
+
+    while True:
+        step += 1
+        if square == step:
+            return abs(x) + abs(y)
+        if (x == y) or (x < 0 and x == -y) or (x > 0 and x == 1-y):
+            dx, dy = -dy, dx
+        x, y = x+dx, y+dy
+
+def steps_adjacent(square):
+    x = y = dx = 0
+    dy = -1
+    grid = {}
+    coords = [(1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1)]
+
+    while True:
+        total = 0
+        for offset in coords:
+            ox, oy = offset
+            if (x+ox, y+oy) in grid:
+                total += grid[(x+ox, y+oy)]
+        if total > int(square):
+            return total
+        if (x, y) == (0, 0):
+            grid[(0, 0)] = 1
+        else:
+            grid[(x, y)] = total
+        if (x == y) or (x < 0 and x == -y) or (x > 0 and x == 1-y):
+            dx, dy = -dy, dx
+        x, y = x+dx, y+dy
--- 2017/python/tests/Day03/test_spiral.py
+++ 2017/python/tests/Day03/test_spiral.py
@@ -1,11 +1,16 @@
-from Day03.spiral import steps_carried
+from Day03.spiral import steps_carried, steps_adjacent
 
 def test_steps_carried():
     
     assert steps_carried(1) == 0
     assert steps_carried(12) == 3
-    # assert steps_carried(23) == 2
-    # assert steps_carried(1024) == 31
+    assert steps_carried(23) == 2
+    assert steps_carried(1024) == 31
     
-    # assert steps_carried(368078) == 371
+    assert steps_carried(368078) == 371
+    
+def test_steps_adjacent():
+    
+    assert steps_adjacent(1) == 2
+    assert steps_adjacent(368078) == 369601
     
