d4f08a73c1a17f6be1b59b6edfac82f3a6d16793
Sten Johnsen <sten.johnsen@bouvet.no>
2023-10-01 22:59:43 +0200

    Day04 part 2

--- 2017/python/Day04/passphrase.py
+++ 2017/python/Day04/passphrase.py
@@ -1,5 +1,5 @@
 
-def passphrase(phrase):
+def isValid(phrase):
     parts = phrase.split(' ')
     for i in range(len(parts)):
         if parts[i] in parts[i+1:]:
@@ -10,7 +10,30 @@ def passphrase(phrase):
 def countValidPassPhrases(phrases):
     count = 0
     for phrase in phrases:
-        if passphrase(phrase.strip()):
+        if isValid(phrase.strip()):
+            count += 1
+            
+    return count
+
+def sortString(str):
+    return ''.join(sorted(str))
+
+def isAnagram(first, second):
+    return sortString(first) == sortString(second)
+
+def hasAnagram(phrase):
+    parts = phrase.strip().split(' ')
+    for i in range(len(parts)):
+        for j in range(len(parts[i+1:])):
+            if isAnagram(parts[i], parts[i+j+1]):
+                return True
+    
+    return False    
+
+def countValidWithoutAnagrams(phrases):
+    count = 0
+    for phrase in phrases:
+        if isValid(phrase) and hasAnagram(phrase) == False:
             count += 1
             
     return count
--- 2017/python/tests/Day04/test_passphrase.py
+++ 2017/python/tests/Day04/test_passphrase.py
@@ -1,11 +1,11 @@
 
-from Day04.passphrase import passphrase,countValidPassPhrases
+from Day04.passphrase import isValid,countValidPassPhrases, isAnagram, hasAnagram, countValidWithoutAnagrams
 
 def test_passphrase():
     
-    assert passphrase('aa bb cc dd ee') == True
-    assert passphrase('aa bb cc dd aa') == False
-    assert passphrase('aa bb cc dd aaa') == True
+    assert isValid('aa bb cc dd ee') == True
+    assert isValid('aa bb cc dd aa') == False
+    assert isValid('aa bb cc dd aaa') == True
     
     assert countValidPassPhrases(['aa bb cc dd ee', 'aa bb cc dd aa', 'aa bb cc dd aaa']) == 2
     
@@ -15,3 +15,18 @@ def test_passphrase():
     assert countValidPassPhrases(data) == 325
     
     
+def test_isAnagram():
+    assert isAnagram('abcde', 'ecdab') == True
+    assert isAnagram('abcde', 'ecddb') == False
+    
+def test_hasAnagram():
+    assert hasAnagram('abcde fghij') == False
+    assert hasAnagram('abcde xyz ecdab') == True
+    assert hasAnagram('a ab abc abd abf abj') == False
+    assert hasAnagram('oiii ioii iioi iiio') == True
+    
+def test_countValidWithoutAnagrams():
+    inputData = open('../data/input4.txt','r')
+    data = inputData.readlines()
+    
+    assert countValidWithoutAnagrams(data) == 119
