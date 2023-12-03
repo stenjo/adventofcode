e7bab7c07d192b2c81058995be3ff9e819bf8759
Sten Johnsen <sten@thinea.no>
2023-12-03 09:46:37 +0100

    Day03 part 2

--- 2023/python/Day03/PartNumbers.py
+++ 2023/python/Day03/PartNumbers.py
@@ -3,23 +3,34 @@ import re
 class Schematic:
     def __init__(self,schema):
         self.schema = schema
+        self.starMap = {}
         
-    def partNumbers(self, lines):
+    def partNumbers(self, lines= None):
+        if lines == [] or lines == None:
+            lines = self.schema
         nums = []
         for lineNo in range(len(lines)):
             start = None
             for idx in range(len(lines[lineNo])):
                 if lines[lineNo][idx].isdigit() == False:
-                    if start != None and self.checkForSymbols(lines, lineNo, start, idx):
-                        nums.append(int(lines[lineNo][start:idx]))
+                    self.addNums(lines, nums, lineNo, start, idx)
                     start = None
                 else:
                     if start == None:
                         start = idx
-            if start != None and self.checkForSymbols(lines, lineNo, start, idx):
-                nums.append(int(lines[lineNo][start:len(lines[lineNo])]))
+            self.addNums(lines, nums, lineNo, start, len(lines[lineNo]))
             
         return nums
+
+    def addNums(self, lines, nums, lineNo, start, idx):
+        if start != None and self.checkForSymbols(lines, lineNo, start, idx):
+            number = int(lines[lineNo][start:idx])
+            nums.append(number)
+            for star in self.hasAdjacentStar(lines, lineNo, start, idx):
+                if star not in self.starMap.keys():
+                    self.starMap[star]= [number]
+                else:
+                    self.starMap[star].append(number)
     
     def hasSymbol(self, lines, lineNo, start, end):
         if lineNo < 0: return False
@@ -32,6 +43,26 @@ class Schematic:
             return False
         
         return True
+
+    def getStarIndexes(self, line):
+        return [i for i, letter in enumerate(line) if letter == '*']
+
+
+    def hasStar(self, lines, lineNo, start, end):
+        if lineNo < 0: return []
+        if lineNo >= len(lines): return []
+            
+        if start > 0: start -= 1
+        if end < len(lines[lineNo]): end += 1
+        
+        stars = []
+        starIndexes = self.getStarIndexes(lines[lineNo][start:end:])
+        if starIndexes != []:
+            for p in starIndexes:
+                stars.append((lineNo,p+start))
+            return stars
+        
+        return []
     
     def checkForSymbols(self, lines, lineNo, start, end):
         
@@ -39,3 +70,15 @@ class Schematic:
             if self.hasSymbol(lines, l, start, end):
                 return True
             
+    def hasAdjacentStar(self, lines, lineNo, start, end):
+        stars = []
+        for l in range( lineNo -1, lineNo+ 2):
+            stars += self.hasStar(lines, l, start, end)
+        return stars
+    
+    def getGearRatioSum(self):
+        sum = 0
+        for nums in self.starMap.values():
+            if len(nums) == 2:
+                sum += nums[0]*nums[1]
+        return sum
--- 2023/python/tests/Day03/test_PartNumbers.py
+++ 2023/python/tests/Day03/test_PartNumbers.py
@@ -39,3 +39,29 @@ def test_get_partNumbers():
     inputData = open("../data/input03.txt", "r").readlines()
     assert sum(Schematic(lines).partNumbers(inputData)) == 530495
     
+def test_adjacentStar():
+    assert Schematic([".1."]).partNumbers() == []
+    assert Schematic([".1."]).getStarIndexes("...*...") == [3]
+    assert Schematic([".1."]).getStarIndexes("...*.*.") == [3,5]
+    
+    assert Schematic([]).hasStar(["....",".1..","..."], 1, 0,4) == []
+    assert Schematic([]).hasStar(["....",".*..","..."], 1, 0,4) == [(1,1)]
+    assert Schematic([]).hasStar(["*...",".*.*","..."], 1, 0,4) == [(1,1),(1,3)]
+    assert Schematic([]).hasStar(["*...",".*.*","..."], 0, 0,4) == [(0,0)]
+    assert Schematic([]).hasStar(["*...",".*.*","..."], 1, 3,4) == [(1,3)]
+    
+    assert Schematic([]).hasAdjacentStar(["*...","....","...."], 1, 3,4) == []
+    assert Schematic([]).hasAdjacentStar(["*...","...*","...."], 1, 0,4) == [(0,0),(1,3)]
+    
+    s = Schematic(lines[0:3])
+    assert s.partNumbers() == [467,35]
+    assert s.starMap == {(1, 3): [467, 35]}
+    
+    s = Schematic(lines)
+    s.partNumbers()
+    assert s.getGearRatioSum() == 467835
+    
+    inputData = open("../data/input03.txt", "r").readlines()
+    s = Schematic(inputData)
+    s.partNumbers()
+    assert s.getGearRatioSum() == 80253814
