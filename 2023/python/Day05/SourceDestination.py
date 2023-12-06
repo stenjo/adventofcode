18981e8b4b4c237bb312c7f2037afb34a34ac84b
Sten Johnsen <sten.johnsen@bouvet.no>
2023-12-06 05:32:37 +0100

    wip better solution

--- 2023/python/Day05/SourceDestination.py
+++ 2023/python/Day05/SourceDestination.py
@@ -19,3 +19,16 @@ class SourceDestination:
             if source >= s and source < s + c:
                 return source - s + d
         return source
+
+    def getDestRange(self):
+        r = []
+        for dest, count in self.mapping.items():
+            r.append(range(dest, dest + count))
+
+        return r.sort()
+
+    def getSource(self, dest):
+        for s, (d, c) in self.mapping.items():
+            if dest >= d and dest < d + c:
+                return dest - d + s
+        return dest
