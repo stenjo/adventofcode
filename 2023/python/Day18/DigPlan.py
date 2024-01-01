f61634cdbb53d59dde7074744859f057f7175d80
Sten Johnsen <sten.johnsen@bouvet.no>
2024-01-01 08:01:09 +0100

    Completed and refactored Day18

--- 2023/python/Day18/DigPlan.py
+++ 2023/python/Day18/DigPlan.py
@@ -1,131 +1,67 @@
 from itertools import pairwise
-from rich import print
 import matplotlib.pyplot as plt
 
-Point = tuple[int,int]
+Point = tuple[int, int]
+
 
 class DigPlan:
-    def __init__(self,input):
-        self.plan = [(a[0], int(a[1]), a[2]) for a in [line.strip().split() for line in input]]
-        self.map = {}
-        self.dim = {"minY": 0, "minX": 0, "maxY": 0, "maxX": 0}
-        self.plot = []
-        
+    def __init__(self, input):
+        self.plan = [
+            (a[0], int(a[1]), a[2]) for a in [line.strip().split() for line in input]
+        ]
+
+    def convFromHex(self, plan):
+        converted = []
+
+        for line in plan:
+            (_, _, hexStr) = line
+            dir = "RDLU"[int(hexStr[-2])]
+            length = int(hexStr[2:-2], 16)
+            converted.append((dir, length, hexStr))
+
+        return converted
+
     def dig(self, start, inst):
-        directions = {"R": (1,0), "L":(-1,0), "U":(0,-1), "D": (0,1)}
-        (x,y) = start
+        directions = {"R": (1, 0), "D": (0, 1), "L": (-1, 0), "U": (0, -1)}
+        (x, y) = start
         holes: list[Point] = []
         (dir, length, _) = inst
-        (dX,dY) = directions[dir]
-        for n in range(1,length+1):
-            holes.append((x+(dX*n), y+(dY*n)))
-            
-        return holes
-            
-    def hexDig(self, start, inst):
-        directions = [(1,0), (0,1), (-1,0), (0,-1)]
-        (x,y) = start
-        holes: list[Point] = []
-        (_, _, hexStr) = inst
-        length = int(hexStr[2:-2], 16)
-        dir = int(hexStr[-2])
-        (dX,dY) = directions[dir]
-        for n in range(1,length+1):
-            holes.append((x+(dX*n), y+(dY*n)))
-            
-        return holes
-            
-    def trench(self) -> list[Point]:
-        
-        pos = (0,0)
-        points: list[Point] = []
-        for inst in self.plan:
-            points+=self.dig(pos, inst)
-            pos = points[-1]
-        
-        points.append((0,0))
-        return points
-            
-    def hexTrench(self) -> list[Point]:
+        (dX, dY) = directions[dir]
+        holes.append((x + (dX * length), y + (dY * length)))
+
+        return holes, length
+
+    def trench(self, instructions) -> list[Point]:
         trenchLength = 0
-        pos = (0,0)
-        points: list[Point] = []
-        for inst in self.plan:
-            points+=self.hexDig(pos, inst)
+        pos = (0, 0)
+        points: list[Point] = [pos]
+        for inst in instructions:
+            p, l = self.dig(pos, inst)
+            points += p
             pos = points[-1]
-        
-        points.append((0,0))
+            trenchLength += l
+
         return points, trenchLength
-            
+
     def getArea(self, trench):
         # Shoelace formula. Get the area from outline of points
-        parts = [x1*y2 - x2*y1 for (x1,y1),(x2,y2) in pairwise(trench)]
+        parts = [x1 * y2 - x2 * y1 for (x1, y1), (x2, y2) in pairwise(trench)]
         return sum(parts) / 2
-    
+
     def fill(self):
-        trench = self.trench()
+        trench, trenchLength = self.trench(self.plan)
         area = self.getArea(trench)
 
         # pick's theorem - find the number of points in a shape given its area
-        fill = int(abs(area) - 0.5*len(trench) +1) + len(trench)
-        
+        fill = int(abs(area) - 0.5 * trenchLength + 1) + trenchLength
+
         return fill
-    
+
     def hexFill(self):
-        trench, trenchLength = self.hexTrench()
+        trench, trenchLength = self.trench(self.convFromHex(self.plan))
         area = self.getArea(trench)
 
         # pick's theorem - find the number of points in a shape given its area
-        fill = int(abs(area) - 0.5*trenchLength +1) + trenchLength
-        
+        fill = int(abs(area) - 0.5 * trenchLength + 1) + trenchLength
+
         return fill
-                
-    def print(self):
-        for (xPoints, yPoints, color) in self.plot:
-            plt.plot(xPoints, yPoints, 'o', color=color)
-        plt.show()
-                        
-GridPoint = tuple[int, int]
-OFFSETS: dict[str, GridPoint] = {
-    "R": (0, 1),
-    "D": (1, 0),
-    "L": (0, -1),
-    "U": (-1, 0),
-}
-
-class Solution:
-    def __init__(self, input):
-        self.input = input
-        
-    def num_points(self, outline: list[GridPoint]) -> int:
-        """
-        the number of the points inside an outline plus the number of points in the outline
-        """
-        # shoelace - find the float area in a shape
-        area = (
-            sum(
-                row1 * col2 - row2 * col1
-                for (row1, col1), (row2, col2) in pairwise(outline)
-            )
-            / 2
-        )
-        # pick's theorem - find the number of points in a shape given its area
-        return int(abs(area) - 0.5 * len(outline) + 1) + len(outline)
-
-
-    def part_1(self) -> int:
-        points: list[GridPoint] = [(0, 0)]
-
-        for line in self.input:
-            direction, distance_str, _ = line.split()
-
-            for _ in range(int(distance_str)):
-                points.append(self.add_points(OFFSETS[direction], points[-1]))
-
-        return self.num_points(points)
-    
-    def add_points(self, a: GridPoint, b: GridPoint) -> GridPoint:
-        """
-        add a pair of 2-tuples together. Useful for calculating a new position from a location and an offset
-        """
-        return a[0] + b[0], a[1] + b[1]
--- 2023/python/tests/Day18/test_DigPlan.py
+++ 2023/python/tests/Day18/test_DigPlan.py
@@ -1,37 +1,30 @@
-
-from Day18.DigPlan import DigPlan, Solution
+from Day18.DigPlan import DigPlan
 
 
 testData = [
-"R 6 (#70c710)",
-"D 5 (#0dc571)",
-"L 2 (#5713f0)",
-"D 2 (#d2c081)",
-"R 2 (#59c680)",
-"D 2 (#411b91)",
-"L 5 (#8ceee2)",
-"U 2 (#caa173)",
-"L 1 (#1b58a2)",
-"U 2 (#caa171)",
-"R 2 (#7807d2)",
-"U 3 (#a77fa3)",
-"L 2 (#015232)",
-"U 2 (#7a21e3)"
+    "R 6 (#70c710)",
+    "D 5 (#0dc571)",
+    "L 2 (#5713f0)",
+    "D 2 (#d2c081)",
+    "R 2 (#59c680)",
+    "D 2 (#411b91)",
+    "L 5 (#8ceee2)",
+    "U 2 (#caa173)",
+    "L 1 (#1b58a2)",
+    "U 2 (#caa171)",
+    "R 2 (#7807d2)",
+    "U 3 (#a77fa3)",
+    "L 2 (#015232)",
+    "U 2 (#7a21e3)",
 ]
-#[(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (1, 6), (2, 6), (3, 6), (4, 6), (5, 6), (5, 5), (5, 4), ...]
-#[(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (5, 0), (5, 1), (5, 2), (5, 3), (5, 4), (5, 4), (4, 4), (4, 4), ...]
+
 
 def test_DigPlan():
- 
     assert DigPlan(testData).plan[0] == ("R", 6, "(#70c710)")
-
-    # assert Solution(testData).part_1() == 62
     assert DigPlan(testData).fill() == 62
-    
+
     inputData = open("../data/input18.txt", "r").readlines()
     assert DigPlan(inputData).fill() == 39194
-    
-    
+
     assert DigPlan(testData).hexFill() == 952408144115
     assert DigPlan(inputData).hexFill() == 78242031808225
-    
