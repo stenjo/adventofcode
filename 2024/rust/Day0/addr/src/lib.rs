cdb99d1a233cfef526b8c403642cdd5ca58f3e40
Sten Johnsen <sten@thinea.no>
2024-11-18 07:10:01 +0100

    starting rust

---
+++ 2024/rust/Day0/addr/src/lib.rs
@@ -0,0 +1,26 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
