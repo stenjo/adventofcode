874fd8ccb01cd591f607ea326b16ee45c281969b
Sten Johnsen <sten@thinea.no>
2024-11-22 17:15:09 +0100

    day2

--- 2015/rust/day02/src/lib.rs
+++ 2015/rust/day02/src/lib.rs
@@ -1,7 +1,12 @@
 
-pub fn part1(measures: &str) -> u64 {
-    let [l,w,h] = measures.split("x").collect().as_slice();
-    return 7
+pub fn part1(measures: &str) -> usize {
+    let parts: Vec<usize> = measures.split('x').map(|x| x.parse().expect("Invalid number")).collect();
+    let (l, w, h) = (parts[0], parts[1], parts[2]);
+
+    let f = l * w;
+    let s = w * h;
+    let t = h * l;
+    return 2 * f + 2 * s + 2 * t + f.min(s).min(t);
 }
 
 #[cfg(test)]
@@ -17,8 +22,10 @@ mod tests {
 
     #[rstest]
     #[case::size_1x1x1("1x1x1", 7)]
-    #[case::size_2x1x1("2x1x1", 9)]
-    fn package(#[case] measures: &str, #[case] expected: u64) {
+    #[case::size_2x1x1("2x1x1", 11)]
+    #[case::size_2x3x4("2x3x4", 52)]
+    #[case::size_1x1x10("1x1x10", 42)]
+    fn package(#[case] measures: &str, #[case] expected: usize) {
         assert_eq!(expected, part1(measures));
     }
 }
