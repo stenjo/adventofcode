09a067646a24c88e783590587c5b5786d2881096
Sten Johnsen <sten@thinea.no>
2024-12-01 21:54:13 +0100

    wip day07 2015

--- 2017/rust/day07/src/main.rs
+++ 2017/rust/day07/src/main.rs
@@ -1,48 +1,79 @@
-use std::borrow::BorrowMut;
-use std::cell::RefCell;
-use std::fs;
-use std::ops::Deref;
-use std::rc::Rc;
+use petgraph::{graph::NodeIndex, Graph};
+use std::{borrow::Borrow, collections::HashMap, fs};
 
 #[derive(Debug)]
-pub struct Node {
-    id: String,
-    children: Vec<Rc<RefCell<Node>>>,
-    parents: Vec<Rc<RefCell<Node>>>,
+struct NodeData {
+    name: String,
+    weight: i32,
 }
 
-impl Node {
-    // Constructor for a new Node
-    pub fn new(id: &str) -> Rc<RefCell<Self>> {
-        Rc::new(RefCell::new(Node {
-            id: id.to_string(),
-            children: Vec::new(),
-            parents: Vec::new(),
-        }))
-    }
+fn parse_node(input: &str) -> (String, i32, Vec<String>) {
+    // Split the input into parts by " -> "
+    let parts: Vec<&str> = input.split(" -> ").collect();
 
-    pub fn set_parent(&mut self, other: Rc<RefCell<Node>>) {
-        self.parents.push(other);
-    }
-    // Add a connection to another node
-    pub fn set_child(&mut self, other: Rc<RefCell<Node>>) {
-        self.children.push(other.clone()); // Add the child to this node
+    // Extract name and weight from the first part
+    let main_part = parts[0]; // "fwft (72)"
+    let mut main_iter = main_part.split_whitespace();
+    let name = main_iter.next().unwrap().to_string(); // First word is the name
+    let weight = main_iter.next().unwrap(); // Second part is the weight in parentheses
+    let weight = weight
+        .trim_matches(|c| c == '(' || c == ')')
+        .parse::<i32>()
+        .unwrap();
 
-        // Borrow the other node mutably to update its parents field
-        other
-            .borrow_mut()
-            .set_parent(Rc::new(RefCell::new(self.deref())));
-    }
-    pub fn children(&mut self) -> usize {
-        return self.children.len();
-    }
-    fn parents(&mut self) -> usize {
-        return self.parents.len();
-    }
+    // Extract children from the second part, if it exists
+    let children = if parts.len() > 1 {
+        parts[1].split(", ").map(|s| s.to_string()).collect()
+    } else {
+        Vec::new() // No children if " -> " is not present
+    };
+
+    (name, weight, children)
 }
 
-pub fn part1(input: String) -> i64 {
-    return input.len().try_into().unwrap();
+pub fn part1(input: String) -> String {
+    let mut graph = Graph::<NodeData, ()>::new();
+    let mut index: HashMap<String, NodeIndex> = HashMap::new();
+
+    for line in input.lines() {
+        let mut root_idx: NodeIndex;
+        let mut root: &NodeData;
+        let (name, weight, children) = parse_node(line);
+        if let Some(&root_idx) = index.get(&name) {
+            root = &graph[root_idx];
+
+            println!("Found node: {:?} (index: {})", root, root_idx.index());
+        } else {
+            root_idx = graph.add_node(NodeData {
+                name: name.to_string(),
+                weight: weight,
+            });
+            index.insert(name, root_idx);
+            if !children.is_empty() {
+                for child in children {
+                    // Retrieve or create child node
+                    let child_idx = if let Some(&child_idx) = index.get(&child) {
+                        child_idx
+                    } else {
+                        // Add new node to the graph if it doesn't exist
+                        let new_child_idx = graph.add_node(NodeData {
+                            name: child.to_string(),
+                            weight: 0,
+                        });
+                        index.insert(child.clone(), new_child_idx); // Insert the new node into the index
+                        new_child_idx
+                    };
+
+                    // Add edge from root to child
+                    graph.add_edge(root_idx, child_idx, ());
+                }
+            }
+        }
+    }
+    let roots: Vec<_> = graph.externals(petgraph::Direction::Incoming).collect();
+    let mut r = roots.iter().map(|&i| graph[i].borrow()).collect::<Vec<_>>();
+
+    return r.pop().expect("msg").name.to_string();
 }
 
 pub fn part2(input: String) -> i64 {
@@ -52,23 +83,60 @@ pub fn part2(input: String) -> i64 {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use petgraph::Graph;
     use rstest::rstest;
 
     #[test]
     fn node_test() {
-        let node1 = Node::new("node1");
-        let node2 = Node::new("node2");
+        let mut graph = Graph::<&str, ()>::new();
 
-        node1.borrow_mut().set_child(node2.clone());
-        let child_count = node1.borrow_mut().children();
-        assert_eq!(1, child_count);
-        let parent_count = node2.borrow_mut().parents();
-        assert_eq!(1, parent_count);
+        // Add nodes
+        let root = graph.add_node("root");
+        let child1 = graph.add_node("child1");
+        let child2 = graph.add_node("child2");
+
+        // Add edges (directed)
+        graph.add_edge(root, child1, ());
+        graph.add_edge(root, child2, ());
+
+        // Identify root (a node with no incoming edges)
+        let roots: Vec<_> = graph.externals(petgraph::Direction::Incoming).collect();
+        let r = roots
+            .iter()
+            .map(|&i| graph[i])
+            .collect::<Vec<_>>()
+            .join(":");
+
+        assert_eq!("root", r);
+    }
+
+    #[test]
+    fn parse_node_test() {
+        let input = "fwft (72) -> ktlj, cntj, xhth";
+        let (name, weight, _children) = parse_node(input);
+
+        assert_eq!("fwft", name);
+        assert_eq!(72, weight);
     }
 
     #[rstest]
-    #[case::first("data", 4)]
-    fn test1(#[case] input: String, #[case] result: i64) {
+    #[case::first(
+        "pbga (66)
+    xhth (57)
+    ebii (61)
+    havc (66)
+    ktlj (57)
+    fwft (72) -> ktlj, cntj, xhth
+    qoyq (66)
+    padx (45) -> pbga, havc, qoyq
+    tknk (41) -> ugml, padx, fwft
+    jptl (61)
+    ugml (68) -> gyxo, ebii, jptl
+    gyxo (61)
+    cntj (57)",
+        "tknk"
+    )]
+    fn test1(#[case] input: String, #[case] result: String) {
         assert_eq!(result, part1(input));
     }
 }
