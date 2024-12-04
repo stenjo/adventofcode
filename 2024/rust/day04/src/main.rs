e09fb510f7e44b7c3ce3b9197e2157123ee6f853
Sten Johnsen <sten@thinea.no>
2024-12-04 17:37:29 +0100

    wip

--- 2024/rust/day04/src/main.rs
+++ 2024/rust/day04/src/main.rs
@@ -1,18 +1,17 @@
+use grid::*;
 use std::fs;
 
 pub fn part1(input: String) -> i64 {
-    let mut xword: Vec<Vec<char>> = Vec::new();
+    let mut x: Vec<Vec<char>> = Vec::new();
     for line in input.lines() {
-        xword.push(line.chars().collect());
+        x.push(line.trim().chars().collect());
     }
     let mut count: i64 = 0;
-    count += search_lines(xword.clone());
-    count += search_lines(transpose(xword.clone()));
-    let right_diags = diagonals(xword.clone(), 'r');
-    count += search_lines(right_diags.clone());
-    count += search_lines(transpose(right_diags.clone()));
-    let left_diags = diagonals(xword.clone(), 'l');
-    count += search_lines(left_diags.clone());
+    count += search_lines(&x);
+    count += search_lines(&transpose(x.clone()));
+    count += search_lines(&transpose(diagonals(x.clone(), 'r')));
+    count += search_lines(&transpose(diagonals(x.clone(), 'l')));
+
     return count;
 }
 
@@ -60,14 +59,14 @@ fn diagonals(xword: Vec<Vec<char>>, dir: char) -> Vec<Vec<char>> {
     return xword_diagonals;
 }
 
-fn search_lines(xword: Vec<Vec<char>>) -> i64 {
+fn search_lines(xword: &Vec<Vec<char>>) -> i64 {
     let mut count = 0;
     for i in 0..xword.len() {
         let line: String = xword[i].iter().collect();
         if line.contains("XMAS") {
             count += 1;
         }
-        if xword[i].iter().rev().collect::<String>().contains("XMAS") {
+        if line.contains("SAMX") {
             count += 1;
         }
     }
@@ -75,7 +74,39 @@ fn search_lines(xword: Vec<Vec<char>>) -> i64 {
 }
 
 pub fn part2(input: String) -> i64 {
-    return input.len().try_into().unwrap();
+    let mut grid: Grid<char> = Grid::new(0, 0);
+    for line in input.lines() {
+        grid.push_row(line.chars().collect());
+    }
+
+    let mut count: i64 = 0;
+    for x:i32 in 0..grid.rows() as i32 {
+        for y:i32 in 0..grid.cols() as i32 {
+            let c = grid.get(x, y).unwrap();
+            if *c == 'A' {
+                if (check('M', x + 1, y, &grid) && check('S', x - 1, y, &grid))
+                    || (check('M', x - 1, y, &grid) && check('S', x + 1, y, &grid))
+                    || (check('M', x, y + 1, &grid) && check('S', x, y - 1, &grid))
+                    || (check('M', x, y - 1, &grid) && check('S', x, y - 2, &grid))
+                {
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    return count as i64;
+}
+
+fn check(c: char, x: usize, y: usize, grid: &Grid<char>) -> bool {
+    if (x > 0) && (y > 0) && x < grid.rows() && y < grid.cols() {
+        if let Some(c2) = grid.get(x, y) {
+            if c == *c2 {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 #[cfg(test)]
@@ -90,20 +121,8 @@ mod tests {
     #[case(".A", 'l', ".A")]
     #[case("A.", 'r', "A.")]
     #[case("A.", 'l', "A.")]
-    #[case(
-        "A.
-    .B",
-        'r',
-        ".A.
-    .B."
-    )]
-    #[case(
-        "A.
-    .B",
-        'l',
-        "A..
-    .B."
-    )]
+    #[case("A.\n.B", 'r', ".A.\n.B.")]
+    #[case(".A\nB.\n.X", 'l', ".A..\n.B..\n...X")]
     fn test_diag(#[case] input: String, #[case] dir: char, #[case] out: String) {
         let mut xword: Vec<Vec<char>> = Vec::new();
         for line in input.lines() {
@@ -118,7 +137,113 @@ mod tests {
         assert_eq!(out, output);
     }
 
-    // #[rstest]
+    #[rstest]
+    #[case(
+        "....XXMAS.
+.SAMXMS...
+...S..A...
+..A.A.MS.X
+XMASAMX.MM
+X.....XA.A
+S.S.S.S.SS
+.A.A.A.A.A
+..M.M.M.MM
+.X.X.XMASX",
+        5
+    )]
+    #[case(
+        "....XXS...
+.S..M..A.X
+.A.AA.S.M.
+.MS.S..A.X
+XX.AA.S.M.
+XM..M..A.X
+MSAMXXS.MM
+A..S.A.A.A
+S...M.S.MS
+...XMASAMX",
+        3
+    )]
+    #[case(
+        "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX",
+        5
+    )]
+    #[case(
+        "MMAMXXSSMM
+MSMSMXMAAX
+MAXAAASXMM
+SMSMSMMAMX
+XXXAAMSMMA
+XMMSMXAAXX
+MSAMXXSSMM
+AMASAAXAMA
+SSMMMMSAMS
+MAMXMASAMX",
+        3
+    )]
+    #[case(
+        "M.........
+MM........
+ASM.......
+MMAS......
+XSXMX.....
+XMASXX....
+SXAMXMM...
+SMASAMSA..
+MASMASAMS.
+MAXMMMMASM
+.XMASXXSMA
+..MMMAXAMM
+...XMASAMX
+....AXSXMM
+.....XMASA
+......MMAS
+.......AMA
+........SM
+.........X",
+        5
+    )]
+    #[case(
+        ".........M
+........SA
+.......ASM
+......MMMX
+.....XSAMM
+....XMASMA
+...SXMMAMS
+..MMXSXASA
+.MASAMXXAM
+MSXMAXSAMX
+MMASMASMS.
+ASAMSAMA..
+MMAMMXM...
+XXSAMX....
+XMXMA.....
+SAMX......
+SAM.......
+MX........
+M........",
+        5
+    )]
+    fn test_search(#[case] input: String, #[case] result: i64) {
+        let mut xword: Vec<Vec<char>> = Vec::new();
+        for line in input.lines() {
+            xword.push(line.chars().collect());
+        }
+        let count = search_lines(&xword);
+        assert_eq!(result, count);
+    }
+
+    #[rstest]
     // #[case(vec![
     //     vec!['A', 'B', 'X', '.'],
     //     vec!['S', 'A', 'M', 'X'],
@@ -132,50 +257,50 @@ mod tests {
     //     vec!['A', '.', '.', '.'],
     //     vec!['.', '.', '.', '.'],
     // ])]
-    // #[case(vec![
-    //     vec!['A', 'B', 'X', '.'],
-    //     vec!['S', 'A', 'M', 'X'],
-    //     vec!['G', 'H', 'A', '.'],
-    //     vec!['G', 'H', 'S', '.'],
+    #[case(vec![
+        vec!['A', 'B', 'X', '.'],
+        vec!['S', 'A', 'M', 'X'],
+        vec!['G', 'H', 'A', '.'],
+        vec!['G', 'H', 'S', '.'],
 
-    // ],  'l',
-    //  vec![
-    //     vec!['A', 'B', 'X', '.'],
-    //     vec!['.', 'S', 'A', 'M'],
-    //     vec!['.', '.', 'G', 'H'],
-    //     vec!['.', '.', '.', 'G'],
-    // ])]
-    // #[case(
-    //  vec![
-    //     vec!['X', '.', '.', '.', '.'],
-    //     vec!['.', 'M', '.', '.', '.'],
-    //     vec!['.', '.', 'A', '.', '.'],
-    //     vec!['.', '.', '.', 'S', '.'],
-    // ], 'r',      vec![
-    //     vec!['X', '.', '.', '.', '.'],
-    //     vec!['M', '.', '.', '.', '.'],
-    //     vec!['A', '.', '.', '.', '.'],
-    //     vec!['S', '.', '.', '.', '.'],
-    // ])]
-    // #[case(
-    //  vec![
-    //     vec!['.', '.', '.', 'X', '.'],
-    //     vec!['.', '.', 'M', '.', '.'],
-    //     vec!['.', 'A', '.', '.','.'],
-    //     vec!['S','.', '.', '.',  '.'],
-    // ], 'l',      vec![
-    //     vec!['X', '.', '.', '.', '.'],
-    //     vec!['M', '.', '.', '.', '.'],
-    //     vec!['A', '.', '.', '.', '.'],
-    //     vec!['S', '.', '.', '.', '.'],
-    // ])]
-    // fn test_diagonals(
-    //     #[case] xword: Vec<Vec<char>>,
-    //     #[case] dir: char,
-    //     #[case] result: Vec<Vec<char>>,
-    // ) {
-    //     assert_eq!(result, diagonals(xword, dir));
-    // }
+    ],  'l',
+     vec![
+        vec!['A', 'B', 'X', '.','.', '.', '.' ],
+        vec!['.', 'S', 'A', 'M','X', '.', '.' ],
+        vec!['.', '.', 'G', 'H','A', '.', '.' ],
+        vec!['.', '.', '.', 'G','H', 'S', '.' ],
+    ])]
+    #[case(
+     vec![
+        vec!['X', '.', '.', '.', '.'],
+        vec!['.', 'M', '.', '.', '.'],
+        vec!['.', '.', 'A', '.', '.'],
+        vec!['.', '.', '.', 'S', '.'],
+    ], 'r',      vec![
+        vec!['.', '.', '.', 'X', '.', '.', '.', '.'],
+        vec!['.', '.', '.', 'M', '.', '.', '.', '.'],
+        vec!['.', '.', '.', 'A', '.', '.', '.', '.'],
+        vec!['.', '.', '.', 'S', '.', '.', '.', '.'],
+    ])]
+    #[case(
+     vec![
+        vec!['.', '.', '.', 'X', '.'],
+        vec!['.', '.', 'M', '.', '.'],
+        vec!['.', 'A', '.', '.','.'],
+        vec!['S','.', '.', '.',  '.'],
+    ], 'l',      vec![
+        vec!['.', '.', '.', 'X', '.', '.', '.', '.'],
+        vec!['.', '.', '.', 'M', '.', '.', '.', '.'],
+        vec!['.', '.', '.', 'A', '.', '.', '.', '.'],
+        vec!['.', '.', '.', 'S', '.', '.', '.', '.'],
+    ])]
+    fn test_diagonals(
+        #[case] xword: Vec<Vec<char>>,
+        #[case] dir: char,
+        #[case] result: Vec<Vec<char>>,
+    ) {
+        assert_eq!(result, diagonals(xword, dir));
+    }
 
     // #[rstest]
     // #[case(transpose(vec![
@@ -216,20 +341,20 @@ mod tests {
     //     assert_eq!(xword_transposed, transpose(xword));
     // }
 
-    //     #[rstest]
-    //     #[case(
-    //         "MMMSXXMASM
-    //     MSAMXMSMSA
-    //     AMXSXMAAMM
-    //     MSAMASMSMX
-    //     XMASAMXAMM
-    //     XXAMMXXAMA
-    //     SMSMSASXSS
-    //     SAXAMASAAA
-    //     MAMMMXMMMM
-    //     MXMXAXMASX",
-    //         18
-    //     )]
+    #[rstest]
+    #[case(
+        "MMMSXXMASM
+        MSAMXMSMSA
+        AMXSXMAAMM
+        MSAMASMSMX
+        XMASAMXAMM
+        XXAMMXXAMA
+        SMSMSASXSS
+        SAXAMASAAA
+        MAMMMXMMMM
+        MXMXAXMASX",
+        18
+    )]
     //     #[case(
     //         ".........X
     // .........M
@@ -254,9 +379,9 @@ mod tests {
     //         ...S.",
     //         1
     //     )]
-    //     fn test1(#[case] input: String, #[case] result: i64) {
-    //         assert_eq!(result, part1(input));
-    //     }
+    fn test1(#[case] input: String, #[case] result: i64) {
+        assert_eq!(result, part1(input));
+    }
 }
 
 pub fn main() {
--- 2024/rust/day04/tests/main_test.rs
+++ 2024/rust/day04/tests/main_test.rs
@@ -2,10 +2,10 @@ use assert_cmd::Command;
 
 #[test]
 fn test_part1_success() {
-    Command::cargo_bin("day02")
+    Command::cargo_bin("day04")
         .unwrap()
-        .arg("../../data/day02.txt")
+        .arg("../../data/day04.txt")
         .assert()
         .success()
-        .stdout("321\n386\n");
+        .stdout("2397\n1824\n");
 }
