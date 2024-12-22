da09ad8fc7b8d5aceabdf3a152915c296cf14aa7
Sten Johnsen <sten@thinea.no>
2024-12-23 00:20:14 +0100

    wip day22

--- 2024/rust/day22/src/lib.rs
+++ 2024/rust/day22/src/lib.rs
@@ -9,6 +9,7 @@
 // To mix a value into the secret number, calculate the bitwise XOR of the given value and the secret number. Then, the secret number becomes the result of that operation. (If the secret number is 42 and you were to mix 15 into the secret number, the secret number would become 37.)
 // To prune the secret number, calculate the value of the secret number modulo 16777216. Then, the secret number becomes the result of that operation. (If the secret number is 100000000 and you were to prune the secret number, the secret number would become 16113920.)
 
+use std::cmp::max;
 use std::collections::HashMap;
 
 pub fn calculate_price(secret: u64, iterations: usize) -> u64 {
@@ -23,21 +24,26 @@ pub fn calculate_price(secret: u64, iterations: usize) -> u64 {
         result = result % 16777216;
     }
 
-    return result;
+    result
 }
 
-pub fn sequences(secret: u64) -> HashMap<u64, Vec<u64>> {
-    let mut result: HashMap<u64, Vec<u64>> = HashMap::new();
-    let mut changes: Vec<u64> = vec![];
-    let mut current = calculate_price(secret, 1) % 10;
+pub fn sequences(mut secret: u64) -> HashMap<u64, Vec<i32>> {
+    let mut result: HashMap<u64, Vec<i32>> = HashMap::new();
+    let mut changes: Vec<i32> = vec![];
+    secret = calculate_price(secret, 1);
+    let mut current = secret % 10;
+    changes.push(current as i32);
     for _i in 0..1999 {
-        let next = calculate_price(current, 1) % 10;
-        let change = next - current;
+        secret = calculate_price(secret, 1);
+        let next = secret % 10;
+        let change = next as i32 - current as i32;
         changes.push(change);
-        if changes.len() >= 4 && !result.contains_key(&next) {
-            result.insert(next, changes.clone());
-            if result.len() == 10 {
-                break;
+        if changes.len() == 4 {
+            if !result.contains_key(&next) {
+                result.insert(next, changes.clone());
+                if result.len() == 10 {
+                    break;
+                }
             }
             changes.remove(0);
         }
@@ -45,6 +51,44 @@ pub fn sequences(secret: u64) -> HashMap<u64, Vec<u64>> {
     }
 
     return result;
+}
+
+/// Matches a given pattern in the sequence generated from the secret number.
+///
+/// # Arguments
+///
+/// * `secret` - The initial secret number.
+/// * `pattern` - A vector of i32 representing the pattern to match.
+///
+/// # Returns
+///
+/// * `u64` - The next number in the sequence if the pattern is matched, otherwise 0.
+pub fn match_pattern(mut secret: u64, pattern: Vec<i32>) -> u64 {
+    let mut changes: Vec<i32> = vec![];
+    let mut current = secret % 10;
+    changes.push(current as i32);
+    for i in 0..1999 {
+        secret = calculate_price(secret, 1);
+        let next = secret % 10;
+        let change = next as i32 - current as i32;
+        changes.push(change);
+        if changes.len() >= 4 {
+            if changes[0] == pattern[0]
+                && changes[1] == pattern[1]
+                && changes[2] == pattern[2]
+                && changes[3] == pattern[3]
+            {
+                println!("{} {:?} == {:?}", i, changes, pattern);
+            }
+            if changes == pattern {
+                return next;
+            }
+            changes.remove(0);
+        }
+        current = next;
+    }
+
+    0
 }
 
 pub fn part1(input: String) -> i64 {
@@ -56,17 +100,39 @@ pub fn part1(input: String) -> i64 {
         sum += result;
     }
 
-    return sum as i64;
+    sum as i64
 }
 
 pub fn part2(input: String) -> i64 {
-    let lines = input.lines();
-    let mut sum = 0;
-    for line in lines {
-        let secret = line.parse::<u64>().unwrap();
-        let result = calculate_price(secret, 2000);
-        sum += result;
+    let buyers: Vec<u64> = input.lines().map(|x| x.parse::<u64>().unwrap()).collect();
+    let mut best = 0;
+    for i in 0..buyers.len() {
+        let buyer = buyers[i];
+        let result = check_for(buyer, buyers.clone());
+        if let Some(value) = result {
+            best = max(best, value);
+        }
     }
+    best
+}
 
-    return sum as i64;
+pub fn check_for(starter: u64, buyers: Vec<u64>) -> Option<i64> {
+    let mut best = 0;
+    let startset = sequences(starter);
+    let keys: Vec<&u64> = startset.keys().collect();
+    for i in keys.into_iter().rev() {
+        let mut sum = i.clone() as i64;
+        let pattern = startset[&i].clone();
+        println!("{} : {:?}", i, pattern);
+        for buyer in &buyers {
+            if *buyer == starter {
+                continue;
+            }
+            let seq = match_pattern(*buyer, pattern.clone());
+            println!("{} : {:?}", seq, buyer);
+            sum += seq as i64;
+        }
+        best = max(best, sum);
+    }
+    Some(best as i64)
 }
--- 2024/rust/day22/tests/main_test.rs
+++ 2024/rust/day22/tests/main_test.rs
@@ -18,17 +18,53 @@ const TEST: &str = "1
 2024";
 
 #[rstest]
+#[case(123, 6, vec![-1,-1,0,2])]
+fn test_sequences(#[case] secret: u64, #[case] key: u64, #[case] result: Vec<i32>) {
+    let seq = sequences(secret);
+    assert_eq!(10, seq.len());
+    assert_eq!(result, seq[&key]);
+}
+
+#[rstest]
+#[case(123, vec![-1,-1,0,2], 6)]
+#[case(1, vec![-2,1,-1,3], 7)]
+#[case(2, vec![-2,1,-1,3], 7)]
+#[case(3, vec![-2,1,-1,3], 0)]
+#[case(2024, vec![-2,1,-1,3], 9)]
+fn test_match_pattern(#[case] secret: u64, #[case] pattern: Vec<i32>, #[case] result: u64) {
+    let seq = match_pattern(secret, pattern);
+    assert_eq!(result, seq);
+}
+
+const TEST2: &str = "1
+2
+3
+2024";
+
+#[rstest]
+#[case(2024, vec![1,2,3,2024], 23)]
+fn test_check_for(#[case] secret: u64, #[case] buyers: Vec<u64>, #[case] result: i64) {
+    assert_eq!(result, check_for(secret, buyers).unwrap() as i64);
+}
+
+#[rstest]
+#[case(TEST2, 23)]
+fn test_part2(#[case] input: &str, #[case] result: i64) {
+    assert_eq!(result, part2(input.to_string()) as i64);
+}
+
+#[rstest]
 #[case(TEST, 37327623)]
 fn test_part1(#[case] input: &str, #[case] result: i64) {
     assert_eq!(result, part1(input.to_string()) as i64);
 }
 
-#[test]
-fn test_part1_success() {
-    Command::cargo_bin("day22")
-        .unwrap()
-        .arg("../../data/day22.txt")
-        .assert()
-        .success()
-        .stdout("14869099597\n386\n");
-}
+// #[test]
+// fn test_part1_success() {
+//     Command::cargo_bin("day22")
+//         .unwrap()
+//         .arg("../../data/day22.txt")
+//         .assert()
+//         .success()
+//         .stdout("14869099597\n14869099597\n");
+// }
