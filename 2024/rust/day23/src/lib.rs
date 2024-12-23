ce2610c8f947ed64a97fe5713020c45d877bc84d
Sten Johnsen <sten@thinea.no>
2024-12-23 09:38:34 +0100

    part 2 wip

---
+++ 2024/rust/day23/src/lib.rs
@@ -0,0 +1,100 @@
+use std::collections::{BTreeSet, HashMap, HashSet, VecDeque};
+type Graph = HashMap<String, HashSet<String>>;
+
+fn add_edge(graph: &mut Graph, node1: &str, node2: &str) {
+    graph
+        .entry(node1.to_string())
+        .or_default()
+        .insert(node2.to_string());
+    graph
+        .entry(node2.to_string())
+        .or_default()
+        .insert(node1.to_string());
+}
+
+fn parse_input(input: &str) -> Graph {
+    let mut graph = Graph::new();
+    for line in input.lines() {
+        let mut parts = line.split("-");
+        let node = parts.next().unwrap();
+        let neighbor = parts.next().unwrap();
+        add_edge(&mut graph, node, neighbor);
+    }
+    return graph;
+}
+
+fn find_triangles(graph: &Graph) -> HashSet<BTreeSet<String>> {
+    let mut triangles: HashSet<BTreeSet<String>> = HashSet::new();
+    for (node, neighbors) in graph {
+        for neighbor1 in neighbors {
+            for neighbor2 in neighbors {
+                if neighbor1 < neighbor2 && graph[neighbor1].contains(neighbor2) {
+                    let mut triangle: BTreeSet<String> = BTreeSet::new();
+                    triangle.insert(node.to_string());
+                    triangle.insert(neighbor1.clone());
+                    triangle.insert(neighbor2.clone());
+                    triangles.insert(triangle);
+                }
+            }
+        }
+    }
+    triangles
+}
+
+pub fn bfs_longest(graph: &Graph, start: &str) -> (String, i64, HashSet<String>) {
+    let mut visited: HashSet<String> = HashSet::new();
+    let mut queue: VecDeque<(String, i64)> = VecDeque::from(vec![(start.to_string(), 0)]);
+    let mut parents: HashMap<String, String> = HashMap::new();
+    visited.insert(start.to_string());
+    let mut furthest = (start.to_string(), 0i64);
+    while !queue.is_empty() {
+        if let Some((node, distance)) = queue.pop_front() {
+            if distance > furthest.1 {
+                furthest = (node.clone(), distance as i64);
+            }
+            visited.insert(node.clone());
+            for neighbor in &graph[&node] {
+                if !visited.contains(neighbor) {
+                    queue.push_back((neighbor.clone(), distance + 1));
+                    parents.insert(neighbor.clone(), node.clone());
+                }
+            }
+        }
+    }
+    let mut computers: HashSet<String> = HashSet::new();
+    let mut current: String = furthest.0.clone();
+    computers.insert(current.to_string());
+    while let Some(node) = parents.get(&current) {
+        computers.insert(node.clone());
+        current = node.clone();
+    }
+
+    return (furthest.0, furthest.1, computers);
+}
+
+pub fn longest(graph: &Graph) -> HashSet<String> {
+    let start_node = graph.keys().next().unwrap();
+    let (farthest, _, _) = bfs_longest(graph, start_node);
+    let (_, _, nodes) = bfs_longest(graph, farthest.as_str());
+    return nodes;
+}
+pub fn part1(input: String) -> i64 {
+    let graph = parse_input(&input);
+    let triangles = find_triangles(&graph);
+    let mut count = 0;
+    for triangle in triangles {
+        if triangle.iter().any(|v| v.starts_with("t")) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+pub fn part2(input: String) -> String {
+    let graph = parse_input(&input);
+    let computers = longest(&graph);
+    let mut password_vec: Vec<_> = computers.iter().cloned().collect();
+    password_vec.sort();
+    let password = password_vec.join(",");
+    return password;
+}
---
+++ 2024/rust/day23/tests/main_test.rs
@@ -0,0 +1,66 @@
+use assert_cmd::Command;
+use day23::*;
+use rstest::rstest;
+
+const EXAMPLE: &str = "kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn";
+
+#[rstest]
+#[case::first(EXAMPLE, 7)]
+fn test1(#[case] input: &str, #[case] result: i64) {
+    assert_eq!(result, part1(input.to_string()));
+}
+
+const EXAMPLE2: &str = "ka-co
+ta-co
+de-co
+ta-ka
+de-ta
+ka-de";
+
+#[rstest]
+#[case(EXAMPLE, "aq,cg,co,de,vc")]
+#[case(EXAMPLE2, "co,de,ka,ta")]
+fn test2(#[case] input: &str, #[case] result: &str) {
+    assert_eq!(result, part2(input.to_string()).as_str());
+}
+
+#[test]
+fn test_part1_success() {
+    Command::cargo_bin("day23")
+        .unwrap()
+        .arg("../../data/day23.txt")
+        .assert()
+        .success()
+        .stdout("321\n386\n");
+}
