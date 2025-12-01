f29d5cf55c24160be669f084d75590ea9a664977
Sten Johnsen <sten@thinea.no>
2025-12-01 07:58:44 +0100

    part 1 day 1

--- 2025/swift/Sources/Day01/Day01.swift
+++ 2025/swift/Sources/Day01/Day01.swift
@@ -1,16 +1,61 @@
 import Foundation
 import AdventCore
 
+struct Dial {
+    var code = 50
+    mutating func turn(instruction: String) -> Dial {
+        let direction = instruction.first!
+        let codeString = instruction.dropFirst()
+        guard let dial = Int(codeString) else {
+            fatalError("Invalid instruction: \(instruction)")
+        }
+        
+        var nextCode: Int
+
+        switch direction {
+        case "L":
+            nextCode = (code - dial)
+        case "R":
+            nextCode  = (code + dial)
+        default:
+            fatalError("Invalid direction: \(direction)")
+        }
+
+        var zeroPassed = 0
+        while (nextCode < 0) {
+            nextCode += 100
+            zeroPassed += 1
+        }
+        zeroPassed += nextCode / 100
+        code = nextCode % 100
+        zero += zeroPassed
+
+        return self
+    }
+    var zero: Int  = 0
+}
+
 struct Day01 {
     static func part1(_ input: String) -> Int {
-        // TODO: Implement part 1 solution
         let lines = input.nonEmptyLines
-        return lines.count
+        var zeroCount = 0
+        var dial = Dial()
+        for line in lines {
+            dial = dial.turn(instruction: line)
+            if dial.code == 0 {
+                zeroCount += 1
+            }
+        }   
+        return zeroCount
     }
     
     static func part2(_ input: String) -> Int {
-        // TODO: Implement part 2 solution
-        return 0
+        let lines = input.nonEmptyLines
+        var dial = Dial()
+        for line in lines {
+            dial = dial.turn(instruction: line)
+        }   
+        return dial.zero
     }
 }
 
--- 2025/swift/Tests/Day01Tests/Day01Tests.swift
+++ 2025/swift/Tests/Day01Tests/Day01Tests.swift
@@ -7,11 +7,48 @@ import AdventCore
 struct Day01Tests {
     
     let sampleInput = """
-    example line 1
-    example line 2
-    example line 3
+    L68
+    L30
+    R48
+    L5
+    R60
+    L55
+    L1
+    L99
+    R14
+    L82
     """
-    
+    @Test("Turn dial")
+    func testTurnDial() async throws {
+        var dial = Dial() // Initialize the dial variable
+        dial = dial.turn(instruction:"L68")
+        #expect(dial.code == (82))
+        dial = dial.turn(instruction: "R48")
+        #expect(dial.code == (30))
+        dial = dial.turn(instruction: "L5")
+        #expect(dial.code == (25))
+        dial = dial.turn(instruction:"R60")
+        #expect(dial.code == (85))
+        }
+
+    @Test func passingZero() async throws {
+        var dial = Dial()
+        dial = dial.turn(instruction: "L55") // 95
+        #expect(dial.zero == 1)
+        dial = dial.turn(instruction: "L1") // 94
+        #expect(dial.zero == 1)
+    dial = dial.turn(instruction: "L99") // 95   
+        #expect(dial.zero == 2)
+        dial = dial.turn(instruction: "R14") // 09
+        #expect(dial.zero == 3)
+        dial = dial.turn(instruction: "R82") // 91
+        #expect(dial.zero == 3) 
+        dial = dial.turn(instruction: "R155") // 46
+        #expect(dial.zero == 5) 
+        dial = dial.turn(instruction: "L200") // 46
+        #expect(dial.zero == 7)
+    }
+
     @Test("Part 1 - Sample Input")
     func testPart1Sample() async throws {
         let result = Day01.part1(sampleInput)
@@ -34,7 +71,7 @@ struct Day01Tests {
     @Test("Part 2 - Sample Input")
     func testPart2Sample() async throws {
         let result = Day01.part2(sampleInput)
-        #expect(result == 0)
+        #expect(result == 6)
     }
     
     @Test("Part 2 - Actual Input")
