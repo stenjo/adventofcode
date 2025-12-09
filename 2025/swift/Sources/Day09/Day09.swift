37d805b8855f42061701d9cbbe73593ece778e9d
Sten Johnsen <sten@thinea.no>
2025-12-09 09:56:12 +0100

    wip

--- 2025/swift/Sources/Day09/Day09.swift
+++ 2025/swift/Sources/Day09/Day09.swift
@@ -6,15 +6,12 @@ struct Day09 {
     {
         let x: Int
         let y: Int
-        let color: Color 
-
-        enum Color {
-            case red
-            case blue
-        }
 
         func area(_ tile: Tile) -> Int {
-            return abs(self.x - tile.x+1) * abs(self.y - tile.y+1)
+            return (abs(self.x - tile.x)+1) * (abs(self.y - tile.y)+1)
+        }
+        func toString() -> String {
+            return "(\(x),\(y))"
         }
     }
     struct Floor
@@ -28,22 +25,60 @@ struct Day09 {
                 let position = line.split(separator: ",").compactMap { Int($0) }
 
                 if position.count == 2 {
-                    let tile = Tile(x: position[0], y: position[1], color: .red)
+                    let tile = Tile(x: position[0], y: position[1])
                     tempTiles.append(tile)
                 }
             }
             self.tiles = tempTiles
-
         }
 
+        func get(x:Int,y:Int)-> Tile? {
+            return tiles.first(where: { $0.x == x && $0.y == y })
+        }
+
+        func isInside(x:Int,y:Int) -> Bool {
+            if get(x: x, y: y) != nil {
+                return true
+            }
+
+            // Check surrounding tiles to see
+            let q1=tiles.filter({$0.x <= x && $0.y <= y})
+            let q2=tiles.filter({$0.x >= x && $0.y <= y})
+            let q3=tiles.filter({$0.x <= x && $0.y >= y})
+            let q4=tiles.filter({$0.x >= x && $0.y >= y})
+            if 
+            q1.count > 0 && q4.count > 0 && q3.count > 0 && q2.count > 0
+            {
+                return true
+            }
+            return false
+        }
 
         func largestRectangle() -> Int {
             var largestArea = 0
+            var corners: (Tile, Tile)? = nil
             for i in 0..<tiles.count-1 {
                 let tile = tiles[i]
-                if tile.color != .red {
-                    continue
+                var area = 0
+                for j in i+1..<tiles.count {
+                    let otherTile = tiles[j]
+                    area = tile.area(otherTile)
+                    if area > largestArea {
+                        largestArea = area
+                        corners = (tile, otherTile)
+                        print("New largest area: \(largestArea) with corners \(tile.toString()) and \(otherTile.toString())")
+                    }
                 }
+            }
+            return largestArea
+        }
+
+        
+
+        func largestAllGreens() -> Int {
+            var largestArea = 0
+            for i in 0..<tiles.count-1 {
+                let tile = tiles[i]
                 var area = 0
                 for j in i+1..<tiles.count {
                     if i == j {
@@ -51,12 +86,69 @@ struct Day09 {
                     }
                     let otherTile = tiles[j]
                     area = tile.area(otherTile)
-                    if area > largestArea {
+                    if area > largestArea && allGreens(tile1: tile, tile2: otherTile) {
                         largestArea = area
                     }
                 }
             }
             return largestArea
+        }
+
+        func allGreens(tile1: Tile, tile2: Tile) -> Bool {
+            let minX = min(tile1.x, tile2.x)
+            let maxX = max(tile1.x, tile2.x)
+            let minY = min(tile1.y, tile2.y)
+            let maxY = max(tile1.y, tile2.y)
+
+            let middleX = abs(maxX - minX) / 2 + minX
+            let middleY = abs(maxY - minY) / 2 + minY
+
+            return !isInside(x: middleX, y: middleY)
+        }
+
+        func printRectangle(tile1: Tile?, tile2: Tile?) {
+            if tile1 == nil || tile2 == nil {
+                print("No rectangle to print")
+                return
+            }
+            let minX = min(tile1!.x, tile2!.x)
+            let maxX = max(tile1!.x, tile2!.x)
+            let minY = min(tile1!.y, tile2!.y)
+            let maxY = max(tile1!.y, tile2!.y)
+
+            for y in 0...maxY+2 {
+                var line = ""
+                for x in 0...maxX+2 {
+                    if get(x: x, y: y) != nil {
+                            line += "#"
+                    } else if x >= minX && x <= maxX && y >= minY && y <= maxY {
+                        line += "O"
+                    } else {
+                        line += "."
+                    }
+                }
+                print(line)
+            }
+        }
+
+        func printFloor() {
+            let maxX = tiles.map { $0.x }.max() ?? 0
+            let maxY = tiles.map { $0.y }.max() ?? 0
+
+            for y in 0...maxY+2 {
+                var line = ""
+                for x in 0...maxX+2 {
+                    if get(x: x, y: y) != nil {
+                            line += "#"
+                    } else if 
+                        isInside(x: x, y: y) {
+                        line += "X"
+                    } else {
+                        line += "."
+                    }
+                }
+                print(line)
+            }
         }
 
     }
@@ -66,8 +158,8 @@ struct Day09 {
     }
     
     static func part2(_ input: String) -> Int {
-        // TODO: Implement part 2 solution
-        return 0
+        let floor = Floor(from: input)
+        return floor.largestAllGreens()
     }
 }
 
--- 2025/swift/Tests/Day09Tests/Day09Tests.swift
+++ 2025/swift/Tests/Day09Tests/Day09Tests.swift
@@ -6,7 +6,7 @@ import AdventCore
 @Suite("Day09 Tests")
 struct Day09Tests {
     
-    let sampleInput = 
+let sampleInput = 
 """
 7,1
 11,1
@@ -18,6 +18,32 @@ struct Day09Tests {
 7,3
 """
     
+    @Test("Inside Tiles")
+    func testInsideTiles() async throws {
+        let floor = Day09.Floor(from: sampleInput)
+        floor.printFloor()
+        #expect(floor.isInside(x: 5, y: 5) == true)
+        #expect(floor.isInside(x: 10, y: 5) == true)
+        #expect(floor.isInside(x: 1, y: 1) == false)
+        #expect(floor.isInside(x: 12, y: 1) == false)
+        #expect(floor.isInside(x: 7, y: 8) == false)
+        #expect(floor.isInside(x: 7, y: 0) == false)
+    }
+
+    @Test("Tile area")
+    func testTileArea() async throws {
+        #expect(Day09.Tile(x: 2, y: 2).area(Day09.Tile(x: 1, y: 1)) == 4)
+        #expect(Day09.Tile(x: 2, y: 1).area(Day09.Tile(x: 1, y: 2)) == 4)
+        #expect(Day09.Tile(x: 2, y: 5).area(Day09.Tile(x: 11, y: 1)) == 50)
+    }
+
+    @Test("Get rectangle")
+    func testGetRectangle() async throws {
+        let floor = Day09.Floor(from: sampleInput)
+        let rect1 = floor.largestRectangle()
+        #expect(rect1 == 50)
+    }
+
     @Test("Part 1 - Sample Input")
     func testPart1Sample() async throws {
         let result = Day09.part1(sampleInput)
@@ -38,7 +64,7 @@ struct Day09Tests {
     @Test("Part 2 - Sample Input")
     func testPart2Sample() async throws {
         let result = Day09.part2(sampleInput)
-        // #expect(result == expectedValue)
+        #expect(result == 24)
         print("Part 2 sample result: \(result)")
     }
     
